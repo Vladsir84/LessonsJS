cbe7dece15c802dfb4edefbbaa3341eac1be97e8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-10-26 19:45:47 +0300

    added

---
+++ lesson2a/task1/index.js
@@ -0,0 +1,10 @@
+const userName = 'John';
+const g1reeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
+
+
