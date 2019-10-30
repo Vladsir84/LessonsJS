98ecd94961a2cfd939c7ee8cb31ffd103a3709fa
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-10-30 14:18:26 +0300

    Add files via upload

---
+++ lesson3/task2/index.js
@@ -0,0 +1,29 @@
+ String(17);
+ String(-17.17);
+ String(false);
+ String(null);
+ String(0);
+ String(0);
+
+ /* === */
+
+ Number('17');
+ Number(true);
+ Number(false);
+ Number(null);
+ Number(undefined);
+ Number('   17   ');
+ Number('      ');
+ Number('   17d   ');
+
+ /* == */
+
+ Boolean(null);
+ Boolean(undefined);
+ Boolean(0);
+ Boolean(-0);
+ Boolean(NaN);
+ Boolean('');
+ Boolean(' ');
+ Boolean(17);
+ Boolean('Hello');
