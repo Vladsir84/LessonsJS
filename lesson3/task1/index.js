5513585b49bd5c06ea9fddf074bde9b134f75551
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-10-30 14:53:05 +0300

    Add files via upload

--- lesson3/task1/index.js
+++ lesson3/task1/index.js
@@ -1,36 +1,62 @@
 // Унарный плюс приводит операнд к числу
-+17; +
++17;
+console.log(+17); +
 '77';
+console.log(+'77');
 
 // Бинарный плюс складывает строки или прибавляет числа
 10 + '5';
+console.log(10 + '5');
 'some' + 'text';
+console.log('some' + 'text');
 
 // Математические операции undefined возаращают NaN
 undefined + 1;
+console.log(undefined + 1);
 null + 8;
+console.log(null + 8);
 undefined + '5';
+console.log(undefined + '5');
 7 - null;
-5 - undefined 'text' + null;
+console.log(7 - null);
+5 - undefined;
+console.log(5 - undefined);
+'text' + null;
+console.log('text' + null);
 let num = 17;
+console.log(17);
 
 // Постфиксная и префиксная форма записи
 ++num;
+console.log(++num);
 num++;
+console.log(num++);
 
 
 // математические операторы приводят операнды к числам
 const a = 17;
+console.log(a = 17);
 const b = -a;
+console.log(b = -a);
 '24' / 4;
+console.log('24' / 4);
 18 * '3';
-'9' - '3'
+console.log(18 * '3');
+'9' - '3';
+console.log('9' - '3');
 'seventeen' / 3;
+console.log('seventeen' / 3);
 4 * 'four';
+console.log(4 * 'four');
 17 / 0;
+console.log(17 / 0);
 
 10 + 'seven';
+console.log(10 + 'seven');
 undefined + 1;
+console.log(undefined + 1);
 null + 8;
+console.log(undefined + '5');
 undefined + '5';
 'text' + null;
+console.log('text' + null);
