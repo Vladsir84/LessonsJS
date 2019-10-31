a91bff10c0d94ddf2c409f3fec24c2342bea972e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-10-31 17:42:49 +0300

    Add files via upload

--- lesson3/task1/index.js
+++ lesson3/task1/index.js
@@ -8,7 +8,7 @@ console.log("+'77': ", +'77');
 10 + '5';
 console.log("10 + 5: ", 10 + '5');
 'some' + 'text';
-console.log('some' + 'text');
+console.log("'some' + 'text'; ", 'some' + 'text');
 
 // Математические операции undefined возаращают NaN
 undefined + 1;
@@ -22,41 +22,42 @@ console.log("7 - null: ", 7 - null);
 5 - undefined;
 console.log("5 - undefined: ", 5 - undefined);
 'text' + null;
-console.log('text' + null);
+console.log("'text' + null: ", 'text' + null);
 let num = 17;
-console.log(17);
+console.log("17: ", 17);
 
 // Постфиксная и префиксная форма записи
 ++num;
-console.log(++num);
+console.log("++num: ", ++num);
 num++;
-console.log(num++);
+console.log("num++: ", num++);
 
 
 // математические операторы приводят операнды к числам
 const a = 17;
-console.log(a = 17);
+console.log("a = 17: ", a = 17);
 const b = -a;
-console.log(b = -a);
+console.log("b = -a: ", b = -a);
 '24' / 4;
-console.log('24' / 4);
+console.log("'24' / 4: ", '24' / 4);
 18 * '3';
-console.log(18 * '3');
+console.log("18 * '3': ", 18 * '3');
 '9' - '3';
-console.log('9' - '3');
+console.log("'9' - '3': ", '9' - '3');
 'seventeen' / 3;
-console.log('seventeen' / 3);
+console.log("'seventeen' / 3: ", 'seventeen' / 3);
 4 * 'four';
-console.log(4 * 'four');
+console.log("4 * 'four': ", 4 * 'four');
 17 / 0;
-console.log(17 / 0);
+console.log("17 / 0: ", 17 / 0);
 
 10 + 'seven';
-console.log(10 + 'seven');
+console.log("10 + 'seven': ", 10 + 'seven');
 undefined + 1;
-console.log(undefined + 1);
+console.log("undefined + 1: ", undefined + 1);
 null + 8;
-console.log(undefined + '5');
+console.log("null + 8: ", null + 8);
 undefined + '5';
+console.log("undefined + '5' ", undefined + '5');
 'text' + null;
-console.log('text' + null);
+console.log("'text' + null ", 'text' + null);
