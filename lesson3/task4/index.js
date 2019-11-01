27ca22d41634bb157fb5cff318e7fcc9202dc9b5
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-01 12:31:44 +0300

    Add files via upload

--- lesson3/task4/index.js
+++ lesson3/task4/index.js
@@ -1,36 +1,48 @@
 // оператор || (OR)
-false || true
-false || true || false
-false || false || false
+console.log("false || true: ", false || true);
+console.log("false || true || false: ", false || true || false);
+console.log("false || false || false: " + false || false || false);
 
 // оператор && (AND)
-true && true
-false && true && false
-true && true && true
+console.log("true && true: " + true && true);
+console.log("false && true && false: " + false && true && false);
+console.log("true && true && true: " + true && true && true);
 
 // Логические операторы могут применяться к любым типам данных
 
 // || находит первое истинное значение
 
-    'text' || false;
+'text' || false;
+console.log("'text' || false: " + 'text' || false);
 true || 'text';
+console.log("true || 'text': " + true || 'text');
 undefined || null || 0 || '' || null;
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
 
 const customAmount = null;
+console.log("const customAmount = null: " + 'const customAmount = null');
 const defaultAmount = 17;
 const amount = customAmount || defaultAmount;
 
 // && находит первое ложное значение
 
 'text' && false;
+console.log("'text' && false: " + 'text' && false);
 true && 'text';
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
 undefined && null && 0 && '' && null;
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
+
 
 /* == */
 
 !true;
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
 !false;
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
 !'text';
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
 !!'text';
+console.log("undefined || null || 0 || '' || null: " + undefined || null || 0 || '' || null);
 
 // !-> && -> ||
