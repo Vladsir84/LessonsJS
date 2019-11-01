344c9091d5856777939113dc2c589855a74c5362
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-01 14:26:25 +0300

    Add files via upload

--- lesson3/task3/index.js
+++ lesson3/task3/index.js
@@ -1,30 +1,40 @@
- console.log('17 to string is ' + String(17));
- console.log('-17.17 to string is ' + String(-17.17));
- console.log('false to string is ' + String(false));
- console.log('null to string is ' + String(null));
- console.log('undefined to string is ' + String(undefined));
- console.log('0 to string is ' + String(0));
+/* сравнение чисел */
+const a = 17;
+const b = 5;
+console.log("a > b: ", a > b);
+console.log("a <= b: ", a <= b);
+console.log("a == b: ", a == b);
+console.log("a != b: ", a != b);
 
+/* сравнение строк */
 
- console.log("'17' to number is " + Number('17'));
- console.log("true to number is " + Number(true));
- console.log("false to number is " + Number(false));
- console.log("null to number is " + Number(null));
- console.log("undefined to number is " + Number(undefined));
- console.log("'   20   ' to number is " + Number('   20   '));
- console.log("'      ' to number is " + Number('      '));
- console.log("'   30d   ' to number is " + Number('   30d   '));
+console.log("'a' < 'b': ", 'a' < 'b');
+console.log("'ab' > 'a': ", 'ab' > 'a');
 
+/* строгое сравнение */
 
- console.log("\ null \  to boolean is " + Boolean(null));
- console.log("\ undefined \ to boolean is " + Boolean(undefined));
- console.log("\ 0 \ to boolean is " + Boolean(0));
- console.log("\ -0 \ to boolean is " + Boolean(-0));
- console.log("\ NaN \ to boolean is " + Boolean(NaN));
- Boolean('');
- //  console.log('\ '\
- //      '   to boolean is ' + Boolean(''));
- //  console.log('\ '\
- //      ' to boolean is ' + Boolean(' '));
- console.log("\ 17 \ to boolean is " + Boolean(17));
- console.log("\'Hello\' to boolean is " + Boolean('Hello'));
+console.log("17 === 1: ", 17 === 1);
+console.log("17 === 17: ", 17 === 17);
+console.log("17 === '17': ", 17 === '17');
+console.log("17 === true: ", 17 === true);
+console.log("'0' === '': ", '0' === '');
+console.log("true === false: ", true === false);
+console.log("true === true: ", true === true);
+console.log("null === undefined: ", null === undefined);
+console.log("false === 0: ", false === 0);
+
+/* нестрогое сравнение */
+
+console.log("17 == '17': ", 17 == '17');
+console.log("'0' == '': ", '0' == '');
+console.log("0 == '': ", 0 == '');
+console.log("null == undefined: ", null == undefined);
+console.log("false == 0: ", false == 0);
+
+/* осторожно c null и undefined */
+
+console.log("undefined == null: ", undefined == null);
+console.log("undefined == 0: ", undefined == 0);
+console.log("null == 0: ", null == 0);
+console.log("undefined < 0: ", undefined < 0);
+console.log("undefined > 0: ", undefined > 0);
