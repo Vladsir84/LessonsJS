e4ad803200f4f2e6c35354d3c3d0da48d60f8624
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-01 12:53:54 +0300

    Add files via upload

--- lesson3/task4/index.js
+++ lesson3/task4/index.js
@@ -4,7 +4,7 @@ console.log("false || true || false: ", false || true || false);
 console.log("false || false || false: ", false || false || false);
 
 // оператор && (AND)
-console.log("true && true: ", true && true);
+console.log("false && true: ", false && true);
 console.log("false && true && false: ", false && true && false);
 console.log("true && true && true: ", true && true && true);
 
@@ -21,7 +21,7 @@ const defaultAmount = 17;
 const amount = customAmount || defaultAmount;
 
 // && находит первое ложное значение
-
+console.log("'text' && false: ", 'text' && false);
 console.log("'text' && false: ", 'text' && false);
 console.log("true && 'text': ", true && 'text');
 console.log("undefined && null && 0 && '' && null: ", undefined && null && 0 && '' && null);
@@ -31,7 +31,7 @@ console.log("undefined && null && 0 && '' && null: ", undefined && null && 0 && 
 
 console.log("!true: ", !true);
 console.log("!false: ", !false);
-console.log("!'text': ", 'text');
+console.log("!'text': ", !'text');
 console.log("!!'text': ", !!'text');
 
 // !-> && -> ||
