cbf88ea1054e5889b5c342c811d883f6a48d9dcc
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-05 17:43:23 +0300

    added

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -1,16 +1,9 @@
-// let n = 1;
+ let n = 200;
+ let sum = 0;
 
-// while (n <= 10) {
-//     console.log(n);
-//     n++;
-// }
+ while (n > 0) {
+     sum += n;
+     n -= 1;
+ }
 
-let n = 100;
-let sum = 0;
-
-while (n <= 200) {
-    sum += n;
-    n -= 1;
-}
-
-console.log('Result: ' + sum);
+ console.log(sum);
