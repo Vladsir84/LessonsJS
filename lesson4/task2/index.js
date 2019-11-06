6c331bab1d08befb39eb24994f3df5fa703c8c6b
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-06 12:55:47 +0200

    Update index.js

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,11 +1,12 @@
 const m = 10;
 const n = 20;
+let counter =1;
 let result = 1;
 
 do {
     if (m % 2 === 1) {
-        result *= m;
+        result *= counter;
    }
- ++m;
+ ++counter;
 } while (m <= n);   
 console.log('Result:' + result);
