6e0f1e41beafcd29985921f4d7f2081de791c18e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-06 15:17:19 +0200

    Update index.js

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -1,9 +1,9 @@
-for (let i = 1; 1 <= 5; i++); {
-    console.log('\n=> ' + i + ' <=\n\n');
-
+for (let i = 1; 1 <= 5; i++) {
+   console.log('\n=> ' + i + ' <=\n\n'); 
+    
     for (let j = 1; j <= 10; j++) {
         console.log(i + ' x ' + j + ' = ' + i * j);
     }
-}
+ }
 
 
