f6d1e9e2dc57824233a1224149e04c32c8c56c51
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-06 13:09:44 +0200

    Update index.js

--- lesson4/task3/index.js
+++ lesson4/task3/index.js
@@ -7,25 +7,3 @@ for (let i = 1; 1 <= 5; i++); {
 }
 
 
-// let result = 0;
-
-// while (true) {
-//     const userInput = prompt('Your number');
-//     if (!userInput) {
-//         break;
-//     }
-//     result += Number(userInput);
-// }
-
-// console.log('Result: ' + result);
-
-
-
-// let result = 1;
-
-// for (let m = 10; m <= 20; m++) {
-//     if (m % 2 === 0) continue;
-//     result *= m;
-// }
-
-// console.log('Result: ' + result);
