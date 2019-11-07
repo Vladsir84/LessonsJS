68eeb6a50f8554128f07b33b7a36306904fd2b2a
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-07 11:55:32 +0200

    Update index.js

--- task7/index.js
+++ task7/index.js
@@ -1,11 +1,5 @@
-const n = 10000000;
 let sum = 0;
-
-let i = n;
-
-while (i > 0) {
+for (let i = 0; i <= 10000000; i++) {
     sum += i;
-    i -= 1;
 }
-
-console.log('Result:' + sum);
+console.log(sum);
