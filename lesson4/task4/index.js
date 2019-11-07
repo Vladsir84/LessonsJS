edf473b11e45c197220fa21e7e9a95ef12caba1b
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-07 13:57:10 +0200

    Update index.js

--- lesson4/task4/index.js
+++ lesson4/task4/index.js
@@ -1,7 +1,7 @@
 const n = 7;
-let sum = ' ';
+let sum = "";
 let i = n;
-for (let i = ' '; i <= 7; i++) {
-    sum += i;
+for (let i = 1; i <= 7; i++) {
+    sum += String(i);
 }
 console.log('Result: ' + "'" + sum + "'");
