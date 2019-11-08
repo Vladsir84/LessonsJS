edb9f9a8c9f637f57e6b21f87ea88331bfa601b1
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-08 12:48:38 +0200

    Update index.js

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -1,7 +1,8 @@
 let sum = 0;
 
 for (let i = 0; i <= 1000; i++) {
-    if (i % 2 === 1) sum += i; {
+    if (i % 2 === 1) {
+        sum += i;
         console.log('Found');
     }
     if ((sum += i) * 5 > 5000) {
