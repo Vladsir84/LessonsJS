2f5b1639905e7887240044fb831a1dbb7c779245
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-08 15:04:54 +0200

    Update index.js

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -3,8 +3,8 @@ let sum = 0;
 for (let i = 0; i <= 1000; i++) {
     if (i % 2 === 1) {
         sum += i;
-        console.log('Found');
     }
+    console.log('Found');
 
     if ((sum += i) * 5 > 5000) {
         console.log('Bigger');
