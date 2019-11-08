2133dabd0368c21a725906ea8c2eb8aa2e87f59e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-08 12:03:06 +0200

    Update index.js

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -1,12 +1,13 @@
 let sum = 0;
 
 for (let i = 0; i <= 1000; i++) {
-    if (i % 2 === 1) {
-        console.log('Found');
-    } else if ((sum += i) * 5 > 5000) {
+    if (i % 2 === 1) sum += i;
+}
+console.log('Found'); {
+    if ((sum += i) * 5 > 5000) {
         console.log('Bigger');
-    } else ((sum += i) * 5 <= 5000){
+    }
+    if ((sum += i) * 5 <= 5000) {
         console.log('Smaller or equal');
     }
-
 }
