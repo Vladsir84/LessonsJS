f7e140562a810c61a05698ae2dcacd29f2d001a8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-09 14:29:09 +0200

    Update index.js

--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -4,9 +4,15 @@ for (let i = 1; i <= 1000; i++) {
     sum += i;
 }
 
+let k = sum / 1234;
+for (let k = 1; k <= 1000; k++) {
+    sum += k;
+}
+
 let j = sum % 1234;
 for (let j = 1; j <= 1000; j++) {
     sum += j;
+
     console.log(true);
     console.log(false);
 }
