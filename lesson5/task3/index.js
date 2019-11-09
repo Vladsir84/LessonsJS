3cf78126677022aeaa1831a0d77969674c0111c5
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-09 18:46:53 +0200

    Update index.js

--- lesson5/task3/index.js
+++ lesson5/task3/index.js
@@ -1,29 +1,9 @@
-/* return statement */
+function getEvenOdd(num) {
+    if (num % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+     }
 
-// function getEvenOdd(num) {
-//     if (num % 2 === 0) {
-//         return 'even';
-//     } else {
-//         return 'odd';
-//     }
-
-//     console.log('Hello');
-// }
-
-// const result = getEvenOdd(20);
-
-// alert(result);
-
-
-const getSum1 = function(a, b) {
-    return a + b;
-};
-
-function getSum2(a, b) {
-    return a + b;
-};
-
-const result1 = getSum1(20, 5);
-const result2 = getSum2(10, 5);
-
-console.log(result1, result2);
+}
+const result = getEvenOdd(20);
