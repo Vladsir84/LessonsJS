317468b2787fe568e45851d9f85d2401b09dd9b5
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-09 14:41:00 +0300

    Add files via upload

---
+++ lesson4/task8/index.js
@@ -0,0 +1,12 @@
+let sum = 0;
+
+for (let i = 1; i <= 1000; i++) {
+    sum += i;
+}
+
+let j = sum / 1234;
+for (let j = 1; j <= 1000; j++) {
+    sum += j;
+    console.log(true);
+    console.log(false);
+}
