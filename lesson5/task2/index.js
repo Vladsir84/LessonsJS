7ec9ce63a227db4fc2108376cb9ab14e365b5a19
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-09 19:28:17 +0300

    Add files via upload

---
+++ lesson5/task2/index.js
@@ -0,0 +1,10 @@
+/* function arguments */
+
+const prefix = 'Hi, ';
+
+function sayHi(name, anotherName = 'anonymus') {
+    const greeting = prefix + name + ' and ' + anotherName;
+    console.log(greeting);
+}
+
+sayHi('Tom', 'Bob');
---
+++ lesson5/task3/index.js
@@ -0,0 +1,29 @@
+/* return statement */
+
+// function getEvenOdd(num) {
+//     if (num % 2 === 0) {
+//         return 'even';
+//     } else {
+//         return 'odd';
+//     }
+
+//     console.log('Hello');
+// }
+
+// const result = getEvenOdd(20);
+
+// alert(result);
+
+
+const getSum1 = function(a, b) {
+    return a + b;
+};
+
+function getSum2(a, b) {
+    return a + b;
+};
+
+const result1 = getSum1(20, 5);
+const result2 = getSum2(10, 5);
+
+console.log(result1, result2);
