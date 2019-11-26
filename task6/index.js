f1685359db84734051e1446b5e4c0f6462274227
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-26 15:30:13 +0200

    Add files via upload

---
+++ task6/index.js
@@ -0,0 +1,9 @@
+function getPrimes(n) {
+    for (let i = 1; i < n; i++) {
+        let a = 0;
+        for (let k = i; k >= 1; k--) {
+            if (k != i && k != 1 && i % k === 0) a++;
+        }
+        if (i === 1 || i === n || a === 0) console.log(i);
+    }
+}
