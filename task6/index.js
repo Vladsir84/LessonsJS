33219fc292fe97f0b11c2d24583727d9ddda5909
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-26 15:31:53 +0200

    Delete index.js

--- task6/index.js
+++
@@ -1,9 +0,0 @@
-function getPrimes(n) {
-    for (let i = 1; i < n; i++) {
-        let a = 0;
-        for (let k = i; k >= 1; k--) {
-            if (k != i && k != 1 && i % k === 0) a++;
-        }
-        if (i === 1 || i === n || a === 0) console.log(i);
-    }
-}
