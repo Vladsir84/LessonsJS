2ce1cdf0b2d50cd767bb67166cf07016184efb51
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-26 16:14:36 +0200

    Add files via upload

---
+++ lesson5/task8/index.js
@@ -0,0 +1,15 @@
+function compareSums(a, b, c, d) {
+    if (sum(a, b) > sum(c + d)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function sum(from, to) {
+    let nSum = 0;
+    for (let i = from; i <= to; i++) {
+        nSum += i;
+    }
+    return nSum;
+}
