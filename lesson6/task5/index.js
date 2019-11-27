b582fff60605b156393c6098dbc90171289c3cfa
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-27 12:21:38 +0200

    Add files via upload

---
+++ lesson6/task5/index.js
@@ -0,0 +1,13 @@
+function squareArray(arr) {
+    let squareNum = [];
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            let squareElem = arr[i] * arr[i];
+            squareArr.push(squareElem);
+        }
+    }
+    return squareNum;
+}
