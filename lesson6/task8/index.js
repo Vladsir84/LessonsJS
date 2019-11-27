3d264129195173fa6627442926d1f4df81c1ee98
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-27 16:37:03 +0200

    Update index.js

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,7 +1,6 @@
 function uniqueCount(arr) {
     let num = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) num++;
-    }
+    let n = arr.filter((item, index) => 
+    index == arr.indexOf(item) ? num++ : false);
     return num;
 }
