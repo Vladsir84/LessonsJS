3bb0c45fa8d5f287a03fb95d796738549a435081
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-27 12:03:22 +0200

    Update index.js

--- lesson6/task3/index.js
+++ lesson6/task3/index.js
@@ -1,32 +1,7 @@
-// const people = ['Tom', 'Ann', 'Bob'];
-
-// people[people.length] = 'Jack';
-
-// people.push('Jack');
-
-// console.log(people);
-// console.log(people.length);
-
-
-// const people = ['Tom', 'Ann', 'Bob'];
-
-// people[people.length] = 'Jack';
-
-// people.unshift('Jack', 'John');
-// people.push('Jack', 'John');
-// const first = people.shift();
-
-// console.log(first);
-// console.log(people);
-// console.log(people.length);
-
-
-// from , to => [n % 3 === 0]
-
-function getSpecialNumbers(from, to) {
+function getSpecialNumbers(m, n) {
     let specialNumbers = [];
 
-    for (let i = from; i <= to; i++) {
+    for (let i = m; i <= n; i++) {
         if (i % 3 === 0) {
             specialNumbers.push(i);
         }
@@ -35,6 +10,3 @@ function getSpecialNumbers(from, to) {
     return specialNumbers;
 }
 
-const specialNumbers = getSpecialNumbers(10, 30);
-
-console.log(specialNumbers);
