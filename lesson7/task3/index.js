bc129b9189b2ec6933581613e6548eea4cd203d0
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 19:39:14 +0200

    Update index.js

--- lesson7/task3/index.js
+++ lesson7/task3/index.js
@@ -1,14 +1,14 @@
 // [1, [2, 3, 4], 5, [6]] => [1, 2, 3, 4, 5, 6]
 
 
-const flatArray = arr =>
-    arr.reduce((acc, elem) => {
-        return acc.concat(elem);
-    }, []);
+// const flatArray = arr =>
+//     arr.reduce((acc, elem) => {
+//         return acc.concat(elem);
+//     }, []);
 
-const initArray = [1, [2, 3, 4], 5, [6]];
+// const initArray = [1, [2, 3, 4], 5, [6]];
 
-console.log(flatArray(initArray));
+// console.log(flatArray(initArray));
 
 
 
@@ -16,9 +16,9 @@ console.log(flatArray(initArray));
 const flatArray = arr => arr
     .flat();
 
-const initArray = [1, [2, 3, 4], 5, [6]];
+// const initArray = [1, [2, 3, 4], 5, [6]];
 
-console.log(flatArray(initArray));
+// console.log(flatArray(initArray));
 
 
 
