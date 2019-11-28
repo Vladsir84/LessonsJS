2b2bab20b6f00f0b84e355c5858730f9c41bc016
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 19:33:11 +0200

    Update index.js

--- lesson7/task2/index.js
+++ lesson7/task2/index.js
@@ -15,11 +15,11 @@
 
 // console.log(sortArray(arr));
 
-const sortArray = numbers =>
+const sortDesc = numbers =>
     numbers.slice().sort((a, b) => b - a);
 
-const arr = [2, 4, 1, 5, 21, 18];
+// const arr = [2, 4, 1, 5, 21, 18];
 
-console.log(sortArray(arr));
+// console.log(sortArray(arr));
 
-console.log(arr);
+// console.log(arr);
