e571be45849a72cca9ce92ba55cbe8fd8c4e9682
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 09:51:08 +0200

    Update index.js

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,3 +1,15 @@
+
+// function uniqueCount(arr) {
+//     let num = 0;
+//     for (i = 0; i < arr.length - 1; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             if (arr[i] !== arr[j]) num++;
+//         }
+//         return num;
+//     }
+// }
+
+
 function uniqueCount(arr) {
     let num = 0;
     let n = arr.filter((item, index) => 
