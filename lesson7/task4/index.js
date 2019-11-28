12bbc15922dfecb9d719780654b7450daba81385
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 20:02:42 +0200

    Update index.js

--- lesson7/task4/index.js
+++ lesson7/task4/index.js
@@ -1,17 +1,6 @@
-// ['Ann', 'Tom', 'Bob', 'Kate']; ['Tom', 'Bob'] => ['Good job, Ann', 'Good job, Kate']
-
-
 const getMessagesForBestStudents = (allStudents, failedStudents) =>
 
     allStudents
     .filter(name => !failedStudents.includes(name))
     .map(name => 'Good job, ' + name);
 
-
-// const allStudents = ['Ann', 'Tom', 'Bob'];
-// const failedStudents = ['Tom', 'Bob'];
-
-// console.log(getMessagesForPassedStudents(allStudents, failedStudents));
-
-// 1. Find passed students
-// 2. Add Good job
