3434d6c2151359ddc07fedce0faba09a5159e09e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 22:41:22 +0200

    Update index.js

--- lesson8/task1/index.js
+++ lesson8/task1/index.js
@@ -1,26 +1,3 @@
-// const prop = 'my'
-
-
-// const obj = {
-//     name: 'Tom',
-//     age: 17,
-//     interest: 'football',
-//     'my name': 'Bob',
-//     [prop + 'name']: 'value',
-// };
-
-// console.log(obj.age); // 17;
-
-// obj.age = 18;
-
-// console.log(obj.age); // 18
-
-// console.log(obj['my name']);
-// console.log(obj[prop + 'name']);
-
-
-// transformToObject
-
 const transformToObject = arr => {
     let obj = {};
 
@@ -30,18 +7,3 @@ const transformToObject = arr => {
 
     return obj;
 };
-
-// [1, 'text'] => {'1': 1, text: 'text'}
-
-// const arr = [1, 'text'];
-// const result = transformToObject(arr);
-
-// console.log(result);
-
-// const user = {
-//     name: 'Tom'
-// };
-
-// user.name = 'Bob';
-
-// console.log(user.name);
