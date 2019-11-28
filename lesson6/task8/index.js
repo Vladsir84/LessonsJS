c21f01a6d77aea83275e72a30677a98a0e684a6a
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 11:33:26 +0200

    Update index.js

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,7 +1,7 @@
 
 function uniqueCount(arr) {
     let counter = 0;
-    arr.sort((a,b) => a - b);
+    arr.sort((m, n) => m - n);
     for (i = 0; i < arr.length; i++) {
         if (arr[i] !== arr[i + 1]) counter++;
      }
