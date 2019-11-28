aea727b19887f43bf9d45c3cd8fc07a7cc2d3cff
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-28 22:46:12 +0200

    Update index.js

--- lesson8/task2/index.js
+++ lesson8/task2/index.js
@@ -1,38 +1,7 @@
-// concatProps
-
-// const arr = ['a', 'b'];
-
-// for (let value of arr) {
-//     console.log(value);
-// }
-
-// const user = {
-//     name: 'Tom',
-//     key: 17,
-// };
-
-// for (let key in user) {
-//     console.log(user[key]);
-// }
-
-
-// user => ['Tom', 17]
-
-
-// const user = {
-//     name: 'Tom',
-//     key: 17,
-// };
-
 const concatProps = obj => {
     let arr = [];
-
     for (let key in obj) {
-        // arr.push(obj[key]);
-        arr = arr.concat(obj[key]);
+    arr = arr.concat(obj[key]);
     }
-
     return arr;
 };
-
-// console.log(concatProps(user));
