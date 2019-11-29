a6fc7e11df146db457c796a3cea930fef4e3bacc
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-11-29 12:03:39 +0200

    Add files via upload

---
+++ lesson8/task3/index.js
@@ -0,0 +1,11 @@
+const pickProps = obj => {
+    let object = {};
+
+    for (let prop in obj) {
+        for (let item of arr)
+
+            if (prop === item) object[prop] = obj[prop];
+    }
+
+    return object;
+};
