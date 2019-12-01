454646ebcfc0d940c809faf92fdffc8c913ece65
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-01 17:34:14 +0200

    Update index.js

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -1,5 +1,5 @@
-const getFiniteNumbers = arr => arr.map(el => Number.isFinite(el));
-const getFiniteNumbersV2 = arr => arr.map(el => isFinite(el));
-const getNaN = arr => arr.map(el => Number.isNaN(el));
-const getNaNV2 = arr => arr.map(el => isNaN(el));
-const getIntegers = arr => arr.map(el => isInteger(el));
+const getFiniteNumbers = arr => arr.filter(el => Number.isFinite(el));
+const getFiniteNumbersV2 = arr => arr.filter(el => isFinite(el));
+const getNaN = arr => arr.filter(el => Number.isNaN(el));
+const getNaNV2 = arr => arr.filter(el => isNaN(el));
+const getIntegers = arr => arr.filter(el => isInteger(el));
