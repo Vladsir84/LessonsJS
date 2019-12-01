7c43e9a3cd234a303fcaa58da15d2eead58f9497
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-01 17:11:17 +0200

    Update index.js

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,4 +1,4 @@
-const getMaxAbsoluteNumber = arr => {
+const getMaxAbsoluteNumber = arr => 
     Math.max(... arr.map(num => Math.abs(num)));
     (!Array.isArray(arr) || arr.length === 0) ? null :
    
