5235ac5a8d73f83e038e5fc3a7f29fa76e7c7e5e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-01 17:28:33 +0200

    Update index.js

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -1,24 +1,5 @@
-Number.isNaN('text'); //
-isNaN('text'); //
-isNaN(NaN); //
-Number.isNaN(NaN); //
-
-Number.isFinite('17'); //
-isFinite('17');
-
-parseInt(' 17.17text'); //
-Number.parseInt(' 17text'); //
-Number.parseInt('seventeen'); //
-
-parseFloat(' 17.17text'); //
-Number.parseFloat(' 17text'); //
-Number.parseFloat('seventeen'); //
-
-Number.isInteger(17); //
-Number.isInteger(17.0); //
-Number.isInteger(17.17); //
-Number.isInteger('17'); //
-Number.isInteger(Infinity); //
-Number.isInteger(NaN); //
-Number.isInteger(undefined); //
-Number.isInteger(null); //
+const getFiniteNumbers = arr => arr.map(el => Number.isFinite(el));
+const getFiniteNumbersV2 = arr => arr.map(el => isFinite(el));
+const getNaN = arr => arr.map(el => Number.isNaN(el));
+const getNaNV2 = arr => arr.map(el => isNaN(el));
+const getIntegers = arr => arr.map(el => isInteger(el));
