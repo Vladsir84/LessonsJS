b8bf7a3c8338fae3fb89820abc11b0f8db1c39b8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-01 16:54:30 +0200

    Update index.js

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,32 +1,8 @@
-// getMaxAbsoluteNumber
-
-// [2, -6, 78, -99] => 99
-// [2, 6, 78, 99]
-
-
-// const getMaxAbsoluteNumber = arr => {
-//     let max = -Infinity;
-
-//     arr.forEach(num => {
-//         if (Math.abs(num) > max) {
-//             max = Math.abs(num);
-//         }
-//     })
-
-//     return max;
-// }
-
-
-const getMaxAbsoluteNumber = arr => {
+/const getMaxAbsoluteNumber = arr => {
     const absoluteValues = arr
         .map(num => Math.abs(num));
-
+    if (!Array.isArray(arr)) {
+        return null;
+    }
     return Math.max(...absoluteValues);
-
-
 }
-
-console.log(getMaxAbsoluteNumber([2, 6, 78, 99]));
-
-// 1 - get absolute values
-// 2 - find max value
