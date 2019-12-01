24458fa82ec87b81f5e9287627017e6424e8b8df
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-01 18:00:09 +0200

    Update index.js

--- lesson10/task2/index.js
+++ lesson10/task2/index.js
@@ -2,6 +2,6 @@ const getParsedIntegers = arr => arr.map(el => Number.parseInt(el));
 
 const getParsedIntegersV2 = arr => arr.map(el => parseInt(el));
 
-const getParsedFloats  = arr => arr.map(el => Number.parseFloat(el));
+const getParsedFloats = arr => arr.map(el => Number.parseFloat(el));
 
 const getParsedFloatsV2 = arr => arr.map(el =>  parseFloat(el));
