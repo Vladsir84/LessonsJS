66abaf8ee0a591f253828ecf0b3e979085af0b9d
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-02 15:16:18 +0200

    Update index.js

--- lesson10/task7/index.js
+++ lesson10/task7/index.js
@@ -1,6 +1,6 @@
-const superRound = (num, elem) => {
+const superRound = (num, el) => {
     let elem = 1;
-    for (let i = 0; i < elem; i++) {
+    for (let i = 0; i < el; i++) {
         elem += '0';
     }
     return [
@@ -8,6 +8,6 @@ const superRound = (num, elem) => {
         (Math.floor(num * elem)) / elem,
         (Math.round(num * elem)) / elem,
         (Math.ceil(num * elem)) / elem,
-        (Math.trunc(num * elem)) / elem, +num.toFixed(elem),
+        (Math.trunc(num * elem)) / elem, +num.toFixed(el),
     ];
 };
