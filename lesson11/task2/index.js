81078181ed31a0255e7331ce659aa7c204cbd934
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-02 17:27:29 +0200

    Update index.js

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -1,37 +1,7 @@
-//sortContacts
-
-const contacts = [{
-        name: 'Tom',
-        phoneNumber: '666-66-66',
-    },
-    {
-        name: 'John',
-        phoneNumber: '555-55-55',
-    },
-    {
-        name: 'Ann',
-        phoneNumber: '333-33-33',
-    },
-    {
-        name: 'Stephan',
-        phoneNumber: '444-44-44',
-    },
-    {
-        name: 'Suzy',
-        phoneNumber: '222-22-22',
-    },
-    {
-        name: 'Adel',
-        phoneNumber: '111-11-11',
-    },
-];
-
-const sortContacts = (contacts, isAsc) => {
-    const result = contacts
-        .sort((a, b) => {
-            return a.name.localeCompare(b.name);
-        });
-    return result;
-};
-
-console.log(sortContacts(contacts));
+const sortContacts = (contacts, isAsc = true) => {
+    if(!Array.isArray(contacts)) return null;
+    return contacts
+        .sort((a, b) => isAsc === true ? 
+        a.name.localeCompare(b.name) :
+        a.name.localeCompare(b.name));    
+    };
