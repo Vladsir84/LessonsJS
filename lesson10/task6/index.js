990bec7ab88926580eaaee9d7f7b5ceb807e6083
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-02 14:04:50 +0200

    Add files via upload

---
+++ lesson10/task6/index.js
@@ -0,0 +1,8 @@
+const getRandomNumbers = (length, min, max) => {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    if (max < min || max - min < 0) return null;
+    return Array(length).fill()
+        .map(() => Math.floor(Math.random() * (max - min) + min));
+};
