abeffe4dc6cfc94bd78e391dea76589b262f9cd7
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-02 15:09:09 +0200

    Add files via upload

---
+++ lesson10/task7/index.js
@@ -0,0 +1,13 @@
+const superRound = (num, elem) => {
+    let elem = 10;
+    for (let i = 0; i < elem; i++) {
+        elem += '0';
+    }
+    return [
+
+        (Math.floor(num * elem)) / elem,
+        (Math.round(num * elem)) / elem,
+        (Math.ceil(num * elem)) / elem,
+        (Math.trunc(num * elem)) / elem, +num.toFixed(elem),
+    ];
+};
