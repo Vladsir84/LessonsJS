6a619b663785ada13aef9924d8bc9ee289dcf78e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-02 17:10:39 +0200

    Update index.js

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,25 +1,3 @@
-//  const text = 'message';
-
-//  const myMessage = `
-//  The text is ${text.toLocaleUpperCase()}, 
-//  And something else. 
-//     `;
-
-
-//  //  const myMessage = 'The text is ${text.toLocaleUpperCase()}, \nAnd something else.';
-
-//  console.log(text[0]); // первый символ строки
-//  console.log(text[text.length - 1]); // последний символ строки
-
-//  console.log(text.charAt(1));
-
-
-//  // splitText
-
-//  // splitString('abcd efgh', 4) => 'Abcd\n efg\nH'...']
-
-//  console.log('Abcd\n efg\nH');
-
 const splitText = (text, len) => {
     if (typeof text !== 'string') return null
     const strArr = [];
@@ -36,7 +14,3 @@ const splitText = (text, len) => {
     return strArr.join('\n');
 };
 
-// 1. 'abcdefg', 4 => ['abcd', 'efg'] => ['Abcd', 'Efg']
-// => 'Abcd/nEfg'
-
-console.log(splitText('abcdefg', 4));
