981a89503d735e8cebcd30540cefe3b91da0b6d2
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-03 09:19:10 +0200

    Update index.js

--- lesson11/task3/index.js
+++ lesson11/task3/index.js
@@ -1,23 +1,3 @@
-// const splitString = (str, len = 10) => {
-//     if (typeof str !== 'string') return null;
-//     let arrLength = Math.ceil(str.length / len);
-//     let startPosition = 0;
-//     return Array(arrLength)
-//         .fill()
-//         .map(() => {
-//             let elem = str.substr(startPosition, len);
-//             startPosition += len;
-//             if (elem.length >= len) return elem;
-//             if (elem.length < len) {
-//                 while (elem.length < len) {
-//                     elem += ".";
-//                 }
-//                 return elem;
-//             }
-//         });
-// };
-
-
 function splitString(str, len = 10) {
     if (typeof str !== 'string') return null;
     let arrString = [];
@@ -35,43 +15,3 @@ function splitString(str, len = 10) {
         return arrString;
     }
 }
-
-// const splitString = (str, len = 10) => {
-//     if (typeof str !== 'string') return null;
-//     let arrLength = Math.ceil(str.length / len);
-//     let startPosition = 0;
-//     return Array(arrLength)
-//         .fill()
-//         .map(() => {
-//             let elem = str.substr(startPosition, len);
-//             startPosition += len;
-//             if (elem.length >= len) return elem;
-//             if (elem.length < len) {
-//                 while (elem.length < len) {
-//                     elem += ".";
-//                 }
-//                 return elem;
-//             }
-//         });
-};
-
-
-// function splitString(string, charNum = 10) {
-//     if (typeof(string) !== 'string') return null;
-
-//     let arrStr = [];
-//     let start = 0;
-
-//     while (true) {
-//         let sbstr = string.substr(start, charNum);
-//         if (sbstr.length === 0) break;
-//         arrStr.push(sbstr);
-//         start += charNum;
-//     }
-
-//     while (arrStr[arrStr.length - 1].length < charNum) {
-//         arrStr[arrStr.length - 1] += '.';
-//     }
-
-//     return arrStr;
-// }
