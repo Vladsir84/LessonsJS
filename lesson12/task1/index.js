53143b3f03e256c98caba40c04f6d746532ce525
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-03 14:30:22 +0200

    Add files via upload

---
+++ lesson12/task1/index.js
@@ -0,0 +1,28 @@
+// calc
+
+// '1 + 2' => '1 + 2 = 3'
+
+const calc = expression => {
+    const [a, operator, b] = expression.split(' ');
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = Number(a) + +b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+
+    }
+
+    return `${expression} = ${result}`;
+};
+
+// console.log(calc('6 - 9'));
