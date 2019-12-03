066046ed9060e0a4dd6aa1773ecfec323383729a
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-03 18:42:35 +0200

    Update index.test.js

--- lesson13a/task2/index.test.js
+++ lesson13a/task2/index.test.js
@@ -1,10 +1,11 @@
 import getSum, { getSquaredNumbers, getOddNumbers } from './calculator.js';
 
-it('should get squaredNumbers', () => {
-    const result = getSquaredNumbers([1, 2, 3]);
-
-    expect(result).toEqual([1, 4, 9]);
+it ('should get squared array', () => {
+    const result = getSquaredArray([1,2,3]);
+    
+    expect(result).toEqual([1,4,9]);
 });
+
 
 it('should keep odd numbers only', () => {
     const result = getOddNumbers([1, 2, 3, 4, 5]);
