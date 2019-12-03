b849052d865926f88ca9525e00863f521f0b85db
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-03 15:01:39 +0200

    Update index.js

--- lesson12/task3/index.js
+++ lesson12/task3/index.js
@@ -3,5 +3,3 @@ const cleanTransactionsList = sum => sum
     .filter(el => !Number.isNaN(el))
     .map(el => '$' + el.toFixed(2));
 
-const sums = [' 1.9 ', '16.4', 17, ' 1 dollar '];
-console.log(cleanTransactionsList(sums));
