f7c8ae38431c92513f6d03c060bfe20bb6193e40
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-05 15:54:34 +0200

    Add files via upload

---
+++ lesson14/task1/index.js
@@ -0,0 +1,24 @@
+// sendMessage
+'use strict';
+
+let message = 'Just learn it!';
+
+function sendMessage(name) {
+    const sender = 'Gromcode';
+
+    console.log(`${name}, ${message} Your ${sender}`);
+}
+
+function setMessage(text) {
+    message = text;
+}
+
+sendMessage('Ann');
+
+// setMessage('Hello!');
+
+// sendMessage('Tom');
+
+// message = 'Hi!';
+
+// sendMessage('Tom');
