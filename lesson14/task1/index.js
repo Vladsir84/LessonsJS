b827eeac7873fb35a37567b819618478949c303a
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-05 16:32:51 +0200

    Update index.js

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-let message = 'Just learn it!';
-
-export function sendMessage(name) {
-    let sender = 'Gromcode';
-    console.log(`${name}, ${message} Your ${sender}`);
+let message = "Just learn it!";
+export function sendMessage (name){
+	let sender = "Gromcode";
+	console.log(`${name}, ${message} Your ${sender}`);
 }
-
-export function setMessage(text) {
-    message = text;
+export function setMessage(text){
+	message = text;
 }
