9b09705f431a463c9485df550d495797357c78b8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-05 18:05:09 +0200

    Update index.js

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -2,7 +2,7 @@
 
 let message = 'Just learn it!';
 export function sendMessage(name){
-	let sender = "Gromcode";
+	let sender = 'Gromcode';
 	console.log(`${name}, ${message} Your ${sender}`);
 }
 export function setMessage(text){
