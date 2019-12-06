363a9e10ba63c10653cc2562112f3dceecdbd7d9
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-06 13:49:56 +0200

    Add files via upload

---
+++ lesson15/task2/index.js
@@ -0,0 +1,36 @@
+// const message = 'Have a good day!';
+
+// function sendMessage(name) {
+
+//     let greeting = 'Hi, ';
+
+//     if (name) {
+
+//         console.log(greeting + name);
+//     } else {
+//         console.log(message);
+//     }
+
+// }
+
+// sendMessage('Ann');
+
+
+const message = 'Have a good day!';
+
+function sendMessage(name) {
+
+    let greeting = 'Hi, ';
+
+    if (name) {
+        const greeting = 'Hello, '
+        const name = 'Tad';
+        console.log(greeting + name);
+    } else {
+
+        console.log(message + ' is a message');
+    }
+
+}
+
+sendMessage('Ann');
