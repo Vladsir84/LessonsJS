c1b659ee2178baf42d3754a37842e24a6f86fb7e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-09 23:40:14 +0200

    Add files via upload

---
+++ lesson17/task4/index.js
@@ -0,0 +1,12 @@
+export const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        return (`${this.firstName} ${this.lastName}`);
+    },
+
+    setFullName(fullName) {
+        let elem = fullName.split(' ');
+        [this.firstName, this.lastName] = elem;
+    }
+}
