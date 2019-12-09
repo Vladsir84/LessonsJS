d2958d2517aff0b95bcc7cc8794a2777c41fc414
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-09 23:41:26 +0200

    Update index.test.js

--- lesson17/task4/index.test.js
+++ lesson17/task4/index.test.js
@@ -5,19 +5,13 @@ it('should get default fullName', () => {
     expect(res).toEqual('John Doe');
 });
 
+it('should get new fullName', () => {
+    let res = user.getFullName('Jim Walsh');
+    expect(res).toEqual('Jim Walsh');
+});
+
 it('should set new firstName', () => {
     user.setFullName('Jim Walsh');
     let res = user.firstName;
     expect(res).toEqual('Jim');
-});
-
-// it('should set new lastName', () => {
-//     user.setFullName('Jim Walsh');
-//     let result = user.lastName;
-//     expect(result).toEqual('Walsh');
-// });
-
-it('should get new fullName', () => {
-    let res = user.getFullName('Jim Walsh');
-    expect(res).toEqual('Jim Walsh');
 });
