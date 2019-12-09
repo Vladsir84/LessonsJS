f73d1dbf31f8171bb7389532d995ba2294c58be4
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-09 22:33:09 +0200

    Update index.js

--- lesson17/task3/index.js
+++ lesson17/task3/index.js
@@ -28,13 +28,13 @@ function defer(func, ms) {
 //     console.log(a + b);
 // };
 
-const user = {
-    name: 'Tom',
-    sayHi() {
-        console.log(`Hi, I'am ${this.name}!`)
-    }
-}
+// const user = {
+//     name: 'Tom',
+//     sayHi() {
+//         console.log(`Hi, I'am ${this.name}!`)
+//     }
+// }
 
-const deferredHi = defer(user.sayHi, 1000);
+// const deferredHi = defer(user.sayHi, 1000);
 
-deferredHi.call({ name: 'Bob' });
+// deferredHi.call({ name: 'Bob' });
