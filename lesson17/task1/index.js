6015b9804f27c4693137f46d65bed59512346543
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-09 22:24:50 +0200

    Update index.js

--- lesson17/task1/index.js
+++ lesson17/task1/index.js
@@ -1,16 +1,3 @@
-// methods
-
-// const user = {
-//     name: 'John',
-//     age: 17,
-//     sayHi: function() {
-//         console.log(`Hi, I am ${this.name}.`);
-//     }
-
-// };
-
-// user.sayHi();
-
 export const user = {
     firstName: 'John',
     lastName: 'Doe',
@@ -18,58 +5,3 @@ export const user = {
         return (`${this.firstName} ${this.lastName}`);
     }
 };
-
-// user.getFullName();
-
-// const func = user.getFullName.bind(user);
-
-// func();
-
-// .bind();
-
-// const user = {
-//     name: 'Doe',
-//     getFullName() {
-//         console.log(this.name);
-//     }
-// };
-
-// // user.getFullName();
-
-// const func = user.getFullName.bind({ name: 'Tom' });
-
-// func();
-
-
-// const user = {
-//     name: 'Doe',
-//     getFullName() {
-//         console.log(this.name);
-//     }
-// };
-
-// // user.getFullName();
-
-// const func = user.getFullName;
-
-// func.call({ name: 'Tom' });
-
-
-// const user = {
-//     name: 'Doe',
-//     sayHi(age, message) {
-//         console.log(`${message}. I'm ${this.name}. I'm ${age} years old`);
-//     }
-// };
-
-// // user.getFullName();
-
-// const func = user.sayHi;
-
-// const anotherUther = {
-//     name: 'Tom'
-// };
-
-// func.apply(anotherUther, [17, 'Hello']);
-
-// func.apply({ name: 'Tom' });
