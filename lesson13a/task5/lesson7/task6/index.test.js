18292701cdde98139211f2f2b274e6a75b9f801b
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 19:50:40 +0200

    Update index.test.js

--- lesson13a/task5/lesson7/task6/index.test.js
+++ lesson13a/task5/lesson7/task6/index.test.js
@@ -1,13 +1,16 @@
 import { reverseArray } from './index';
 
-it('should reversed array', () => {
-
+it('should get null', () => {
     const isNotArray = reverseArray('string');
     expect(isNotArray).toEqual(null);
+});
 
+it('should get null', () => {
     const emptyArray = reverseArray([]);
     expect(emptyArray).toEqual(null);
+});
 
+it('should reversed array', () => {
     const reversedArray = reverseArray([1, 2, 3, 4, 5]);
     expect(reversedArray).toEqual([5, 4, 3, 2, 1]);
 
