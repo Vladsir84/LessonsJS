ba557248db9544cd6f7dd3b9541ad4a5fc115750
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 20:14:09 +0200

    Update index.test.js

--- lesson13a/task5/lesson5/task5/index.test.js
+++ lesson13a/task5/lesson5/task5/index.test.js
@@ -5,12 +5,12 @@ it('should get sum of even numbers in the specified interval', () => {
     expect(result).toEqual(6);
 });
 
-it('should get null - invalid input', () => {
+it('should get null', () => {
     let result = getSum(2, '4');
     expect(result).toEqual(null);
 });
 
-it('should get null - invalid input', () => {
+it('should get null', () => {
     let result = getSum([2], 4);
     expect(result).toEqual(null);
 });
