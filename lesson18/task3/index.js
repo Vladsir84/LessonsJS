b79b03bbe47c6e31d0e81b0dea69ea0a1405f75e
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 16:13:46 +0200

    Update index.js

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,29 +1,5 @@
-// pseudo arrays
-
-function argsSum() {
-    return [].reduce.apply(arguments, [(acc, elem) => {
-        return acc + elem;
-    }, 0]);
+export function sumOfSquares() {
+    return [...arguments]
+        .map(elem => elem * elem)
+        .reduce((acc, elem) => acc + elem, 0);
 }
-
-console.log(argsSum(1, 2, 3, 4));
-
-
-// [].reduce((acc, elem) => {}, 0)
-
-function argsSum() {
-    return [].reduce.call(arguments, (acc, elem) => {
-        return acc + elem;
-    }, 0);
-}
-
-console.log(argsSum(1, 2, 3, 4));
-
-
-function argsSum() {
-    return [...arguments].reduce((acc, elem) => {
-        return acc + elem;
-    }, 0);
-}
-
-console.log(argsSum(1, 2, 3, 4));
