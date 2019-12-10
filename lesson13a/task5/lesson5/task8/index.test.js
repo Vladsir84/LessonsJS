6bce6fa572774330bff8c4d5f0aaf6354b5738e1
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 19:35:14 +0200

    Update index.test.js

--- lesson13a/task5/lesson5/task8/index.test.js
+++ lesson13a/task5/lesson5/task8/index.test.js
@@ -3,10 +3,14 @@ import compareSums from './index';
 it('should get bigger sum', () => {
     const resultTrue = compareSums(2, 4, 4, 4);
     expect(resultTrue).toEqual(true);
+});
 
+it('should get bigger sum', () => {
     const resultFalse = compareSums(4, 4, 2, 4);
     expect(resultFalse).toEqual(false);
+});
 
+it('should get bigger sum', () => {
     const resultNull = compareSums(2, 4, 5);
     expect(resultNull).toEqual(null);
 });
