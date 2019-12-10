a1044274dabb81e80e1bb0b62b07284b49ede910
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 19:54:53 +0200

    Update index.test.js

--- lesson13a/task5/lesson8/task3/index.test.js
+++ lesson13a/task5/lesson8/task3/index.test.js
@@ -1,13 +1,16 @@
 import { pickProps } from './index';
 
-it('should return object of certain props', () => {
-
+it('should get null', () => {
     const isNotObject = pickProps("string", []);
     expect(isNotObject).toEqual(null);
+});
 
+it('should get null', () => {
     const notAnArray = pickProps({}, "string");
     expect(notAnArray).toEqual(null);
+});
 
+it('should return object of certain props', () => {
     const obj = { a: 1, b: 2, c: 3 };
     const arr = ['a', 'c'];
     const resultObj = pickProps(obj, arr);
