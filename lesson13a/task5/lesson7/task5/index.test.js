ee1181d792674a909ff064866cc0dcffe2421515
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 19:46:17 +0200

    Update index.test.js

--- lesson13a/task5/lesson7/task5/index.test.js
+++ lesson13a/task5/lesson7/task5/index.test.js
@@ -1,13 +1,16 @@
 import { squareArray } from './index';
 
-it('should return array of squared numbers', () => {
-
+it('should return null', () => {
     const resultIsNotArray = squareArray('string');
     expect(resultIsNotArray).toEqual(null);
+});
 
+it('should return null', () => {
     const resultOfEmptyArray = squareArray([]);
     expect(resultOfEmptyArray).toEqual(null);
+});
 
+it('should return array of squared numbers', () => {
     const resultOfSquaredNums = squareArray([10, 20, 30]);
     expect(resultOfSquaredNums).toEqual([100, 400, 900]);
 
