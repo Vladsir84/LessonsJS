aa8a3c4664694909064344955ac431cc906937b8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 16:02:14 +0200

    Update index.js

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -1,90 +1,19 @@
 'use strict';
 
-// event (this in arrow functions)
-
-// const obj = {
-//     name: 'Tom',
-//     sayHi() {
-//         console.log(this);
-//     },
-// };
-
-// // obj.sayHi();
-
-// const func = obj.sayHi;
-
-// func();
-
-// console.log(this);
-
-
-// function func() {
-//     console.log(this);
-// }
-
-// func();
-
-// window.user = {
-//   name:
-//   id     // глобальный объект
-// }; 
-
-
-// var a = 1;
-// console.log(window.a);
-
-
-// const user = {
-//     name: 'Tom',
-//     sayHi() {
-//         console.log(this);
-//     },
-// };
-
-// // const func = user.sayHi;
-// // func();
-
-// // setTimeout(user.sayHi.bind(user), 100);
-
-// setTimeout(() => user.sayHi(), 100);
-
-
 export const event = {
     guests: [
-        { name: 'Tom', email: 't@gmail.com', age: 17 },
-        { name: 'Bob', email: 'b@gmail.com', age: 18 },
+        { name: 'John', age: 18, email: 'example@server.com' },
+        { name: 'Bob', age: 17, email: 'example1@server.com' },
     ],
-    message: 'Welcome to the party',
+    message: "Welcome to the party!",
     getInvitations() {
         return this.guests
-            .filter(({ age }) => age >= 18)
-            .map(({ name, email }) => ({
-                text: `Hi ${name}. ${this.message}!`,
-                email,
-            }));
+            .filter(({age}) => age >= 18)
+            .map(({email,name}) => {
+                return {
+                    email,
+                    message: `Dear ${name}! ${this.message}`,
+                }
+            });
     }
-}
-
-// console.log(event.getInvitations());
-
-
-// const event = {
-//     guests: [
-//         { name: 'Tom', email: 't@gmail.com', age: 17 },
-//         { name: 'Bob', email: 'b@gmail.com', age: 18 },
-//     ],
-//     message: 'Welcome to the party',
-//     getInvintations() {
-//         return this.guests
-//             .filter(({ age }) => age >= 18)
-//             .map(function({ name, email }) {
-//                 console.log(this);
-//                 return {
-//                     text: `Hi ${name}. ${this.message}`,
-//                     email
-//                 };
-//             }, event);
-//     }
-// }
-
-// console.log(event.getInvintations());
+};
