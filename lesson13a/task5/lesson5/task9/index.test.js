9a49b1d07f548c05dceddb476283fe785d4aee17
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 20:13:22 +0200

    Update index.test.js

--- lesson13a/task5/lesson5/task9/index.test.js
+++ lesson13a/task5/lesson5/task9/index.test.js
@@ -1,7 +1,17 @@
 import findDivCount from './index';
 
 it('should get count of numbers', () => {
-    const getNumbers = findDivCount(2, 8, 2);
-    expect(getNumbers).toEqual(4);
+    let result = findDivCount(2, 8, 2);
+    expect(result).toEqual(4);
 
+});
+
+it('should get null', () => {
+    let result = findDivCount(undefined, 8, 2);
+    expect(result).toEqual(null);
+});
+
+it('should get null', () => {
+    let result = findDivCount(2, 8, undefined);
+    expect(result).toEqual(null);
 });
