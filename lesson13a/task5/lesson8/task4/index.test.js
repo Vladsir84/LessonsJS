508074079126fe7410356d8d9d695e809a066fce
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-10 20:06:14 +0200

    Update index.test.js

--- lesson13a/task5/lesson8/task4/index.test.js
+++ lesson13a/task5/lesson8/task4/index.test.js
@@ -1,13 +1,16 @@
 import { getAdults } from './index';
 
-it('should object of adults', () => {
-
+it('should get null', () => {
     let isNotObject = getAdults("string");
     expect(isNotObject).toEqual(null);
+});
 
+it('should get null', () => {
     let undefinedObj = getAdults();
     expect(undefinedObj).toEqual(null);
+});
 
+it('should get object of adults', () => {
     let obj = { 'John Doe': 19, 'Tom': 17, 'Bob': 18 };
     let objOfAdults = getAdults(obj);
     expect(objOfAdults).toEqual({ 'John Doe': 19, 'Bob': 18 });
