a1ded195319957ff0e252ade4a1b19ba786d7398
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-11 19:42:30 +0200

    Update index.js

--- lesson19/task1/index.js
+++ lesson19/task1/index.js
@@ -1,3 +1,4 @@
+
 export const vehicle = {
     move() {
         console.log(`${this.name} is moving`);
@@ -11,12 +12,13 @@ export const vehicle = {
 export const ship = {
     name: 'Argo',
     startMachine() {
-        console.log(`${this.name}lifting anchor up`);
+        console.log(`${this.name} lifting anchor up`);
         this.move();
     },
     stopMachine() {
-        console.log(`${this.name}lifting anchor down`);
         this.stop();
+        console.log(`${this.name} lifting anchor down`);
+
     },
 };
 
