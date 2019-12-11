f2f9440564e73967a4a0b3e0d88a66af1e21f962
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-11 19:40:09 +0200

    Update index.js

--- lesson19/task1/index.js
+++ lesson19/task1/index.js
@@ -1,62 +1,24 @@
-// vehicle and ship
-
-// const vehicle = {
-//     move() {
-//         console.log('moving');
-//     },
-//     stop() {
-//         console.log('stopped');
-//     }
-// };
-
-// const ship = {
-//     name: 'Aurora',
-//     hasWheels: false,
-//     LiftAnchorDown() {
-//         console.log('lifting anchor down');
-//     },
-//     LiftAnchorUp() {
-//         console.log('lifting anchor up');
-//     },
-
-//     // __proto__: vehicle
-// };
-
-// // ship.move();
-
-// // console.log(ship);
-
-// Object.setPrototypeOf(ship, vehicle);
-
-// ship.move();
-
-// console.log(ship);
-
-
-const vehicle = {
+export const vehicle = {
     move() {
-        console.log(`${this.name} moving`);
+        console.log(`${this.name} is moving`);
     },
     stop() {
         console.log(`${this.name} stopped`);
-        console.log('stopped');
+
     }
 };
 
-const ship = {
-    name: 'Aurora',
-    hasWheels: false,
-    LiftAnchorDown() {
-        console.log('lifting anchor down');
+export const ship = {
+    name: 'Argo',
+    startMachine() {
+        console.log(`${this.name}lifting anchor up`);
+        this.move();
     },
-    LiftAnchorUp() {
-        console.log('lifting anchor up');
+    stopMachine() {
+        console.log(`${this.name}lifting anchor down`);
+        this.stop();
     },
 };
 
 
 Object.setPrototypeOf(ship, vehicle);
-
-// console.log(ship);
-
-ship.move()
