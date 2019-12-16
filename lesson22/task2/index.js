ffc8584e96c781fecb411ef8d84816ea5f5aabc8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-16 11:56:34 +0200

    Update index.js

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -1,20 +1,12 @@
-const tasks = [
-    { text: 'Buy milk', done: false, date: new Date(), id: Math.trunc(Math.random() * 1000), },
-    { text: 'Pick up Tom from airport', done: false, date: new Date(), id: Math.trunc(Math.random() * 100), },
-    { text: 'Visit party', done: false, date: new Date(), id: Math.trunc(Math.random() * 1000), },
-    { text: 'Visit doctor', done: true, date: new Date(), id: Math.trunc(Math.random() * 1000), },
-    { text: 'Buy meat', done: true, date: new Date(), id: Math.trunc(Math.random() * 1000), },
+        const tasks = [
+    { text: 'Visit party', done: false, date: new Date(), id: Math.floor(Math.random() * 1000), },
+    { text: 'Pick up Tom from airport', done: false, date: new Date(), id: Math.floor(Math.random() * 1000), },
+    { text: 'Buy milk', done: false, date: new Date(), id: Math.floor(Math.random() * 1000), },
+    { text: 'Buy meat', done: true, date: new Date(), id: Math.floor(Math.random() * 1000), },
+    { text: 'Visit doctor', done: true, date: new Date(), id: Math.floor(Math.random() * 1000), },
+
 ];
 
-let createTaskBtn = document.querySelector('.create-task-btn');
-createTaskBtn.addEventListener('click', function() {
-    let task_input = document.querySelector('.task-input');
-    tasks.unshift({ text: task_input.value, done: false, });
-    let allLi = document.querySelector('.list');
-    allLi.innerHTML = '';
-    task_input.value = '';
-    renderListItems(tasks);
-});
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
@@ -30,31 +22,46 @@ const renderListItems = listItems => {
             }
             const checkboxElem = document.createElement('input');
             checkboxElem.setAttribute('type', 'checkbox');
-            checkboxElem.setAttribute('data-id', id);
+            checkboxElem.setAttribute('id', id);
             checkboxElem.checked = done;
             checkboxElem.classList.add('list__item-checkbox');
             listItemElem.append(checkboxElem, text);
+
             return listItemElem;
         });
 
     listElem.append(...listItemsElems);
 
-    let check = document.querySelectorAll('.list__item-checkbox');
 
-    const onToggleTask = element => {
-        const Checkbox = element.target.classList.contains('list__item-checkbox');
-        if (!Checkbox) {
+
+    const addTask = event => {
+
+        const isCheckbox = event.target.classList.contains('list__item-checkbox');
+        if (!isCheckbox) {
             return;
         }
-        const taskData = tasks.find(x => x.id == element.target.dataset.id);
-        Object.assign(taskData, { done: element.target.checked });
-        let allLi = document.querySelector('.list');
-        allLi.innerHTML = '';
+        const taskData = tasks.find(task => task.id == event.target.id);
+        Object.assign(taskData, { done: event.target.checked });
+        const listItem = document.querySelector('.list');
+        listItem.innerHTML = '';
         renderListItems(tasks);
+
     };
 
-    const todoListElem = document.querySelector('.list');
-    todoListElem.addEventListener('click', onToggleTask);
+    const taskList = document.querySelector('.list');
+    taskList.addEventListener('click', addTask);
 };
 
 renderListItems(tasks);
+
+const createTaskBtn = document.querySelector('.create-task-btn');
+createTaskBtn.addEventListener('click', () => {
+    const input = document.querySelector('.task-input');
+    if (!input.value) return;
+    tasks.unshift({ text: input.value, done: false, });
+    const listItem = document.querySelector('.list');
+    listItem.innerHTML = '';
+    input.value = '';
+    renderListItems(tasks);
+
+});
