8a10d0cb150523d5626824ac10fd65a689f05f62
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-17 17:10:35 +0200

    Update index.js

--- lesson24/task4/index.js
+++ lesson24/task4/index.js
@@ -1,12 +1,12 @@
-const studentsBirthDays = students => {
-    let result = {};
-    let birthArray = [...students].map((student) => {
-        let date = student.birthDate.split('/').reverse();
-        [date[1], date[2]] = [date[2], date[1]];
-        date = date.join(', ');
-        student.birthDate = new Date(date);
-        return student;
-    })
+export const studentsBirthDays = students => {
+       let result = {};
+       let birthArray = [...students].map((student) => {
+         let date = student.birthDate.split('/').reverse();
+         [date[1], date[2]] = [date[2], date[1]];
+         date = date.join(', ');
+         student.birthDate = new Date(date);
+         return student;
+     });
 
     birthArray.sort((a, b) => a.birthDate > b.birthDate ? 1 : -1);
 
