46c88dd54ed06ebda71930a3ca4768f69ef722d5
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2019-12-18 10:47:21 +0200

    Add files via upload

---
+++ lesson23/task1remake/index.js
@@ -0,0 +1,53 @@
+const emailInputElem = document.querySelector('#email');
+const passwordInputElem = document.querySelector('#password');
+
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
+
+const isRequired = value => value ?
+    undefined :
+    'Required';
+
+const isEmail = value => value.includes('@') ?
+    undefined :
+    'Should be an email';
+
+const validatorsByField = {
+    email: [isRequired, isEmail],
+    password: [isRequired]
+
+};
+
+const validate = (fieldName, value) => {
+    const validators = validatorsByField[fieldName];
+    return validators
+        .map(validator => validator(value))
+        .filter(errorText => errorText)
+        .join(', ');
+};
+
+const onEmailChange = event => {
+    const errorText = validate('email', event.target.value);
+    emailErrorElem.textContent = errorText;
+}
+
+const onPasswordChange = event => {
+    const errorText = validate('password', event.target.value);
+    passwordErrorElem.textContent = errorText;
+}
+
+emailInputElem.addEventListener('input', onEmailChange);
+passwordInputElem.addEventListener('input', onPasswordChange);
+
+const formElem = document.querySelector('.login-form');
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    const obj = {};
+    const formData = [...new FormData(formElem)]
+        .forEach(item => obj[item[0]] = obj[item[1]]);
+
+    alert(JSON.stringify(formData));
+};
+
+formElem.addEventListener('submit', onFormSubmit);
