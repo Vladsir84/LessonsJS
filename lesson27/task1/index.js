0154ffc8612d7ac6c7f2e40555d67312fe18938a
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-13 15:47:58 +0200

    Update index.js

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,24 +1,3 @@
-    // getLocalStorageData
-
-    localStorage.clear();
-    localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-    localStorage.setItem('name', 'Tom');
-    localStorage.setItem('age', JSON.stringify(17));
-
-    // console.log(JSON.parse(localStorage.getItem('hobbies')));
-
-    // const getLocalStorageData = () => {
-    //     for (let i = 0; i < localStorage.length; i++) {
-    //         localStorage.key(i)
-    //     }
-    // };
-
-    // const getLocalStorageData = () => {
-    //     for (let i of localStorage) {
-    //         localStorage.key(i)
-    //     }
-    // };
-
     const getLocalStorageData = () => {
         return Object.entries(localStorage)
             .reduce((acc, [key, value]) => {
@@ -34,5 +13,3 @@
                 };
             }, {});
     };
-
-    console.log(getLocalStorageData());
