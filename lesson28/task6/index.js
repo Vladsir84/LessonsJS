8e7557c1da56f88203a6ead1f417e58db170658b
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-16 12:18:58 +0200

    Add files via upload

---
+++ lesson28/task6/index.js
@@ -0,0 +1,20 @@
+function fibonacci(num) {
+    if (num < 1) {
+        return 1;
+    }​
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}​
+function maxFibonacci(input) {
+    let index = 0;
+
+    let maxFib = 0;​
+    while (true) {
+        let fib = fibonacci(index);
+        if (fib < input) {
+            maxFib = fib;
+            index++;
+        } else {
+            return maxFib;
+        }
+    }
+}
