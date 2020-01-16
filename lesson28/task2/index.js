47b8fbdb7f826c577af2360cec5a4fe1093263e2
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-16 09:01:52 +0200

    Update index.js

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -20,7 +20,7 @@ const tree = {
     ],
 };
 
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
     const isFavorite = favorites.includes(tree.id);
 
     return {
