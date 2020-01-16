c2a069861f9c665fa170295d6d98187cd088a237
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-16 19:32:46 +0200

    Add files via upload

---
+++ lesson29/task2/index.js
@@ -0,0 +1,19 @@
+// pinger(num, period)
+
+const pinger = (num, period) => {
+    let i = num;
+    console.log('Ping');
+    const interval = setInterval(() => {
+        if (--i > 0) {
+            console.log('Ping');
+        } else {
+            clearInterval(interval);
+        }
+    }, period);
+
+    // setTimeout(() => {
+    //     clearInterval(interval);
+    // }, num * period);
+}
+
+pinger(5, 1000);
---
+++ lesson29/task3/index.js
@@ -0,0 +1,21 @@
+// const main = () => {
+//     console.log('Hi');
+//     setTimeout(function cb() {
+//         console.log('there');
+//     }, 5000);
+//     console.log('JSConfEU');
+// };
+
+// main();
+
+
+const main = () => {
+    console.log('1');
+    setTimeout(() => console.log('2'), 0);
+    console.log('3');
+    setTimeout(() => console.log('4'), 1000);
+    console.log('5');
+};
+
+main();
+console.log('6');
