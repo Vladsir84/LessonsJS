ed49af38fd2fcfaf2c48749bfbb4af25df12bc3c
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-18 14:56:43 +0200

    Update index.js

--- lesson29/task5/index.js
+++ lesson29/task5/index.js
@@ -1,12 +1,3 @@
 export const delay = (delay, callback, context, ...args) => {
     setTimeout(() => callback.apply(context, args), delay);
 };
-
-// const object = {
-//     name: 'Tom',
-//     age: 27,
-// };
-// function someCallback(obj){
-//     console.log(obj);
-// };
-// delay(3000, someCallback, object);
