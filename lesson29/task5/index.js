a784b08c1c0d4c3c27a7c72bd1c11233588a3fae
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-18 14:56:15 +0200

    Add files via upload

---
+++ lesson29/task5/index.js
@@ -0,0 +1,12 @@
+export const delay = (delay, callback, context, ...args) => {
+    setTimeout(() => callback.apply(context, args), delay);
+};
+
+// const object = {
+//     name: 'Tom',
+//     age: 27,
+// };
+// function someCallback(obj){
+//     console.log(obj);
+// };
+// delay(3000, someCallback, object);
