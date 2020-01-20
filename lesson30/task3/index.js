7688660f1f9fca00a5eb8ec4adc216d4a7c368c1
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-20 16:58:52 +0200

    Add files via upload

---
+++ lesson30/task3/index.js
@@ -0,0 +1,17 @@
+export function addImage(imgSrc, callback) {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    function onImageLoaded() {
+        callback(null, { width: 200, height: 100 });
+    }
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => callback('Image load failed'));
+
+};
