233bdc301273c230028c06d06abf6a9b15086894
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-21 13:11:59 +0200

    Add files via upload

---
+++ lesson31/task1/index.js
@@ -0,0 +1,34 @@
+// requestUserData
+
+const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    email: `${userId}`,
+                    userId,
+                });
+            }, 1000);
+        }
+    });
+    return request;
+};
+
+requestUserData('broken')
+    .catch(error => {
+        console.log(error)
+        throw new Error('Error');
+    })
+    // .then(data => 4)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+    .finally(data => console.log('finally'));
+
+// requestUserData('broken-1')
+//     .then(data => console.log(data));
