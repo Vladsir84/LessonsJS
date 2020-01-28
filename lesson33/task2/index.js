e2471d54cb02c3b141afb41b82b28802e5a03e18
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-28 09:14:32 +0200

    Update index.js

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,66 +1,67 @@
 const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const showUserBtnElem = document.querySelector('.name-form__btn');
-const userIdInputEelem = document.querySelector('.id-form__input');
-const userRepoInputEelem = document.querySelector('.repo-form__input');
-const userDaysInputEelem = document.querySelector('.days-form__input');
+
+const userIdInputElem = document.querySelector('.id-form__input');
+const userRepoInputElem = document.querySelector('.repo-form__input');
+const userDaysInputElem = document.querySelector('.days-form__input');
 
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 userAvatarElem.src = defaultAvatar;
-
 
 const fetchUserData = userName => {
     return fetch(`https://api.github.com/users/${userName}`)
         .then(response => response.json());
 };
+
 const renderUserData = userData => {
+
     const { avatar_url, name } = userData;
     userAvatarElem.src = avatar_url;
     userNameElem.textContent = name;
+
 };
+
 const onSearchUser = () => {
-    const userName = userIdInputEelem.value;
+    const userName = userIdInputElem.value;
     fetchUserData(userName)
         .then(userData => renderUserData(userData));
 };
+
 showUserBtnElem.addEventListener('click', onSearchUser);
 
 
-
-
-const forGetUserObject = () => {
-    const days = userDaysInputEelem.value;
-    const userId = userIdInputEelem.value;
-    const repoId = userRepoInputEelem.value;
+const getUserObject = () => { // Значения из инпутов.
+    const userId = userIdInputElem.value;
+    const repoId = userRepoInputElem.value;
+    const days = userDaysInputElem.value;
     getMostActiveDevs({ userId, repoId, days });
 };
-showUserBtnElem.addEventListener('click', forGetUserObject);
+
+showUserBtnElem.addEventListener('click', getUserObject);
 
 
-export const getMostActiveDevs = ({ userId, repoId, days }) => {
-    const object = { userId, repoId, days };
-    let maxCount = 0;
-    const startDate = new Date(new Date().setDate(new Date().getDate() - object.days));
-    fetch(`https://api.github.com/repos/${object.userId}/${object.repoId}/commits?per_page=100`)
-        .then(response => response.json())
-        .then(array => {
-            const resArr = array.map(({ commit: { author: { name, email, date } } }) => ({ name, email, date }))
-                .filter(elem => new Date(elem.date) > startDate)
+export const getMostActiveDevs = ({ days, userId, repoId }) => { // Значения в основной функции как объект
+    const commits = { days };
+    let counter = 0;
+    const startDate = new Date(new Date().setDate(new Date().getDate() - commits.days));
+    fetch(`https://api.github.com/repos/${userId}/${repoId}/commits?per_page=100`) // Запрос
+        .then(response => response.json()) // Преобразование с помощью json
+        .then(arr => {
+            let result = arr.map(({ commit: { author: { name, email, date } } }) => ({ name, email, date })) // получаем дату
+                .filter(item => new Date(item.date) > startDate)
                 .reduce((acc, { email, name }) => {
+
                     const oldCount = acc[email] ? acc[email].count : 0;
-                    return {
-                        ...acc,
+                    return {...acc,
                         [email]: { name, email, count: oldCount + 1 }
                     };
-                }, {});
+                }, {})
+            const array = Object.values(result);
+            array.forEach(elem => {
+                if (elem.count > counter) counter = elem.count;
 
-            const arr = Object.values(resArr);
-            arr.forEach(elem => {
-                if (elem.count > maxCount) {
-                    maxCount = elem.count
-                }
-            })
-            return arr.filter(elem => elem.count === maxCount);
-        })
-
+            });
+            return array.filter(elem => elem.count === counter);
+        });
 };
