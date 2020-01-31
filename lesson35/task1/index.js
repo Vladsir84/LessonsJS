c582b6802e7bf16069641bd5fb7d1e9e44ba44e8
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-31 11:40:25 +0200

    Update index.js

--- lesson35/task1/index.js
+++ lesson35/task1/index.js
@@ -1,30 +1,27 @@
-// try {
-//     const jsonString = '{"name":"Tom"';
-//     const userData = JSON.parse(jsonString);
-//     console.log(userData);
-// } catch (err) {
-//     console.error(err.name);
-// } finally {
-//     console.log('Some actions');
-// }
+window.addEventListener("error", function onUnhandledError (err) {
+    console.error("error", err.message);
+});
 
+let userParsingResult;
 
-// window.addEventListener('error', function onUnhandledError(e) {
-//     console.log(e);
-// })
+try {
+    const user = JSON.parse(userParsingResult);
+    console.log("User data: ", user);
+    userParsingResult = "success";
+} catch(e) {
+    userParsingResult = "error";
+} finally {
+    console.log(`User parsing finished with ${userParsingResult}`);
+}
 
+let productParsingResult;
 
-// try {
-//     const jsonString = '{"name":"Tom"';
-//     throw new ReferenceError();
-//     const userData = JSON.parse(jsonString);
-//     console.log(userData);
-// } catch (err) {
-//     if (err instanceof SyntaxError) {
-//         console.error('Handled');
-//     } else {
-//         throw err;
-//     }
-// } finally {
-//     console.log('Some actions');
-// }
+try {
+    const product = JSON.parse(productParsingResult);
+    console.log("Product data: ", product);
+    productParsingResult = "success";
+} catch(e) {
+    productParsingResult = "error";
+} finally {
+    console.log(`Product parsing finished with ${productParsingResult}`);
+}
