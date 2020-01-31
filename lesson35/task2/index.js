6034f942b8118fa4032211f815d2925e8e2b9e92
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-31 11:58:25 +0200

    Update index.js

--- lesson35/task2/index.js
+++ lesson35/task2/index.js
@@ -1,31 +1,25 @@
-// const successRequest = Promise.resolve({ name: 'Tom' });
+const successRequest = Promise.resolve({ name: "Tom" });
 
-// successRequest
-//     .then(data => {
-//         console.log(data);
-//         throw new Error('Unexpected error');
-//     })
-//     .catch(err => {
-//         console.warn(err.massage);
-//     });
-
-
-
-const failedRequest = Promise.reject(new Error('Fail'));
-
-failedRequest
-    .then(data => {
-        console.log(data);
+successRequest
+    .then(function onSuccess1(data) {
+       console.log(data);
+        throw new Error('Error with data');
     })
-    .catch(err => {
-        console.warn(err.message);
-        // throw err;
-        return Promise.reject(err);
+    .catch(function onError1(error) {
+        console.error("onError1", error.message);
     })
-    .then(data => {
-        console.log(data);
+
+
+const failRequest = Promise.reject(new Error("Something went wrong"));
+
+failRequest
+    .catch(function onError2(error) {
+        console.error("onError2", error.message);
+         throw new Error('Server error');
+    })
+    .then(function onSuccess2(data) {
+        console.log("onSuccess2", data);
+    })
+    .catch(function onError3(error) {
+        console.error("onError3", error.message);
     });
-
-window.addEventListener('unhandledrejection', function(e) {
-    console.log(e.reason.message);
-});
