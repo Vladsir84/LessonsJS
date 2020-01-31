580e9d4409203ddb9295787f9c80faf6caf3b6ae
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-01-31 09:28:56 +0200

    Update index.js

--- lesson35/task1/index.js
+++ lesson35/task1/index.js
@@ -7,22 +7,24 @@
 // } finally {
 //     console.log('Some actions');
 // }
-window.addEventListener('error', function onUnhandledError(e) {
-    console.log(e);
-})
 
 
-try {
-    const jsonString = '{"name":"Tom"';
-    throw new ReferenceError();
-    const userData = JSON.parse(jsonString);
-    console.log(userData);
-} catch (err) {
-    if (err instanceof SyntaxError) {
-        console.error('Handled');
-    } else {
-        throw err;
-    }
-} finally {
-    console.log('Some actions');
-}
+// window.addEventListener('error', function onUnhandledError(e) {
+//     console.log(e);
+// })
+
+
+// try {
+//     const jsonString = '{"name":"Tom"';
+//     throw new ReferenceError();
+//     const userData = JSON.parse(jsonString);
+//     console.log(userData);
+// } catch (err) {
+//     if (err instanceof SyntaxError) {
+//         console.error('Handled');
+//     } else {
+//         throw err;
+//     }
+// } finally {
+//     console.log('Some actions');
+// }
