811246524c4f543277003a3737b740c65e6e94ee
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-02-01 15:34:36 +0200

    Update index.js

--- lesson36/task1/index.js
+++ lesson36/task1/index.js
@@ -1,38 +1,3 @@
-// async function func() {
-//     const promise = new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve(17);
-//         }, 1000);
-//     })
-//     const result = await promise;
-//     console.log(result);
-//     console.log('Hello');
-//     return 1;
-// }
-
-// const result = func();
-// console.log('Hi');
-
-
-// const getUser = async userId => {
-//     try {
-//         const response = await fetch(`https://api.github.com/users/${userId}`);
-//         if (!response.ok) {
-//             return null;
-//         }
-//         const userData = await response.json();
-//         return userData;
-//     } catch (err) {
-//         throw new Error('Failed to fetch user');
-//     }
-// };
-
-// getUser('facebook')
-//     .then(userData => console.log(userData))
-//     .catch(err => alert(err.message));
-
-
-
 export const fetchUser = async userId => {
     try {
         const response = await fetch(`https://api.github.com/users/${userId}`);
