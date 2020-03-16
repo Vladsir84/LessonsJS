f4e5ad0fce58c6aa8fb5e0e5cf7d2937bb5b5d91
John Doe <vperedz@gmail.com>
2020-03-17 00:19:56 +0200

    add

--- lesson7/task11/index.js
+++ lesson7/task11/index.js
@@ -1,7 +1,6 @@
 const arr = ['John', 'Olivya', 'Olexandr', 'Emmanuel', 'Vanya', 'Nastya'];
 
-const filterNames = (arr, text) => {
-arr.filter(arr => arr.length > 5)
-arr.filter(arr => arr.text.includes('ya')) 
-}
+const filterNames = (arr, text) => 
+arr.filter(arr => arr.length > 5 && arr.includes(text));
+
 console.log(filterNames(arr, 'ya'));
