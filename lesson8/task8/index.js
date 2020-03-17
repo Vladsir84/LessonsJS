e935eedb00df1b7efff21944d5a1e50dd2109313
John Doe <vperedz@gmail.com>
2020-03-17 17:30:11 +0200

    add

--- lesson8/task8/index.js
+++ lesson8/task8/index.js
@@ -1,33 +1,33 @@
-const user = {
-    name: 'Sam'
-}
+// const user = {
+//     name: 'Sam'
+// }
 
-function addPropretyV1(userData, userId) {
+function addPropertyV1(userData, userId) {
     userData.id = userId;
     return userData
 }
 
-function addPropretyV2(userData, userId) {
+function addPropertyV2(userData, userId) {
     userData.id = userId;
     const obj1 = Object.assign({}, userData)
 
     return obj1
 }
 
-function addPropretyV3(userData, userId) {
+function addPropertyV3(userData, userId) {
     const obj1 = Object.assign({}, userData)
     obj1.id = userId;
     return obj1
 }
 
-function addPropretyV4(userData, userId) {
+function addPropertyV4(userData, userId) {
     const obj1 = { ...userData }
     obj1.id = userId;
     return obj1
 }
 
-//  const result = addPropretyV1(user, '1234567');
+//  const result = addPropertyV1(user, '1234567');
 //  console.log(result);
 
-const result = addPropretyV4(user, '1234567');
-console.log(result);
+// const result = addPropertyV4(user, '1234567');
+// console.log(result);
