967fdc1c13ea995f5244309d0e4ff3be5b677032
John Doe <vperedz@gmail.com>
2020-03-17 09:59:38 +0200

    add

--- lesson7/task9/index.js
+++ lesson7/task9/index.js
@@ -1,5 +1,5 @@
 function withdraw(clients, balances, client, amount) {
-let clientId = clients.findIndex(item => (item === client));
-return balances[clientId] = amount > balances[clientId] ?
-    -1 : balances[clientId] - amount;
+    let clientId = clients.findIndex(item => (item === client));
+    return balances[clientId] = amount > balances[clientId] ?
+        -1 : balances[clientId] - amount;
 };
