937ba9877f1cf33bd8357bfe8204d04990e3cf96
John Doe <vperedz@gmail.com>
2020-03-17 08:40:16 +0200

    add

--- lesson7/task9/index.js
+++ lesson7/task9/index.js
@@ -1,18 +1,5 @@
-let sum = 0;
-let index, costs;
-
-const withdraw = (clients, balances, client, amount) => {
-  clients.map(client => clients)
-
-    for (let i = 0; i < clients.length; i++) {
-        if (clients[i] === client) {
-            index = i;
-            break;
-        }
-    }
-    costs = balances[index];
-    sum = costs - amount;
-    if (sum <= 0) {
-        return -1;
-    } else return sum;
-}
+function withdraw(clients, balances, client, amount) {
+let clientId = clients.findIndex(item => (item === client));
+return balances[clientId] = amount > balances[clientId] ?
+    -1 : balances[clientId] - amount;
+};
