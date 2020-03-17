c3b4d9713021630e1ade7cdc82249649140dca89
John Doe <vperedz@gmail.com>
2020-03-17 17:00:03 +0200

    add

--- lesson8/task8/index.js
+++ lesson8/task8/index.js
@@ -1,3 +1,26 @@
-function addPropretyV1 (userData, userId){
-    return 
+const user = {
+    name: 'Sam'
 }
+
+function addPropretyV1 (userData, userId) {
+   userData.id = userId; 
+    return userData
+}
+
+// function addPropretyV2 (userData, userId) {
+//     let obj = userId.id; 
+//      return obj
+//  }
+
+//  function addPropretyV3 (userData, userId) {
+//     let obj = userId.id; 
+//      return obj
+//  }
+
+//  function addPropretyV4 (userData, userId) {
+//     let obj = userId.id; 
+//      return obj
+//  }
+
+ const result = addPropretyV1(user, '1234567');
+ console.log(result);
