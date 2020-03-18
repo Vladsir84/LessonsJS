de7648e0e143397c4b6378f62a7916667b7bd434
John Doe <vperedz@gmail.com>
2020-03-18 10:23:16 +0200

    add

---
+++ lesson8/task10/index.js
@@ -0,0 +1,34 @@
+const obj1 = {
+    name: 'Tom',
+    age: 17,
+};
+
+const obj2 = {
+    name: 'Bob',
+    student: false,
+};
+
+function mergeObjectsV1(obj1, obj2) {
+   let obj3 = Object.assign(obj1, obj2)
+    return obj3
+}
+
+function mergeObjectsV2(obj1, obj2) {
+ let obj3 = Object.assign(obj2, obj1);
+   return obj3
+}
+
+function mergeObjectsV3(obj1, obj2) {
+    let obj3 = { ...obj1, ...obj2 } 
+       return obj3
+    }
+
+function mergeObjectsV4(obj1, obj2) {
+     obj3 = { ...obj2, ...obj1 }
+        return obj3
+}
+
+ 
+ console.log(mergeObjectsV4(obj1, obj2));
+
+
