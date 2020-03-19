7572d45f5a1cd01b6ccb19ec1a70b30a934f3d20
John Doe <vperedz@gmail.com>
2020-03-19 15:07:24 +0200

    add

--- lesson9/task2/index.js
+++ lesson9/task2/index.js
@@ -25,40 +25,54 @@
 
 
 
-const user = {
-    name: 'Tom',
-    age: 17,
-};
+// const user = {
+//     name: 'Tom',
+//     age: 17,
+// };
 
 // const player = {...user };
 
 // console.log(player);
 
-const { name = 'default', ...restProps } = user;
+// const { name = 'default', ...restProps } = user;
 
 
-const player = {
-    game: 'football',
-    ...restProps
+// const player = {
+//     game: 'football',
+//     ...restProps
+// }
+
+// console.log(player);
+
+// //methods
+
+// const user = {
+//     name: 'Tom',
+//     age: 17,
+//     sayHi: function() {
+//         console.log('Hi');
+//     }
+// };
+
+// user.sayHi();
+
+// // [1, 2].forEach
+
+// // console.log('text'.toUpperCase());
+
+// console.log(1..toFixed());
+// const copyObj = obj => Object.assign({}, obj);
+
+
+const obj = {
+    'John Dee': 19,
+    'Tom': 17,
+    'Bob': 18,
 }
 
-console.log(player);
-
-//methods
-
-const user = {
-    name: 'Tom',
-    age: 17,
-    sayHi: function() {
-        console.log('Hi');
-    }
-};
-
-user.sayHi();
-
-// [1, 2].forEach
-
-// console.log('text'.toUpperCase());
-
-console.log(1..toFixed());
-const copyObj = obj => Object.assign({}, obj);
+const copyObj = obj => {
+    const newObj = {...obj};
+    return newObj
+ } 
+ 
+console.log(copyObj(obj));
