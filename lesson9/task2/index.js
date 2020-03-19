5e99555804a63810061d851a9821883c4c4396ef
John Doe <vperedz@gmail.com>
2020-03-19 15:10:46 +0200

    add

--- lesson9/task2/index.js
+++ lesson9/task2/index.js
@@ -6,64 +6,6 @@
 // };
 
 
-// const players = {
-//     'John Doe': 19,
-//     'Bob': 18,
-// };
-
-// const res = Object.assign(users, players);
-
-// console.log(res);
-
-// res.Tom = 90;
-
-// console.log(users);
-
-// const copyObj = obj => Object.assign({}, obj);
-
-// console.log(copyObj(players));
-
-
-
-// const user = {
-//     name: 'Tom',
-//     age: 17,
-// };
-
-// const player = {...user };
-
-// console.log(player);
-
-// const { name = 'default', ...restProps } = user;
-
-
-// const player = {
-//     game: 'football',
-//     ...restProps
-// }
-
-// console.log(player);
-
-// //methods
-
-// const user = {
-//     name: 'Tom',
-//     age: 17,
-//     sayHi: function() {
-//         console.log('Hi');
-//     }
-// };
-
-// user.sayHi();
-
-// // [1, 2].forEach
-
-// // console.log('text'.toUpperCase());
-
-// console.log(1..toFixed());
-// const copyObj = obj => Object.assign({}, obj);
-
-
 const obj = {
     'John Dee': 19,
     'Tom': 17,
