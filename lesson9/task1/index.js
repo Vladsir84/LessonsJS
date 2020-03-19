2e3a5bbe54c1fe7daa1dd92d961123188cd30f76
John Doe <vperedz@gmail.com>
2020-03-19 14:57:25 +0200

    add

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -1,49 +1,11 @@
-// getAdults
+const obj = {
+    'John Dee': 19,
+    'Tom': 17,
+    'Bob': 18,
+}
+    
+const getAdults = (userObj) => Object.entries(userObj)
+     .filter(obj => obj[1] >= 18)
+     .map(obj => obj[0]);
 
-// const user = {
-//     name: 'Tom',
-//     key: 17,
-// };
-
-
-// Object.keys();
-// Object.values();
-// Object.entries();
-
-// console.log(Object.keys(user));
-
-// const users = {
-//     'Tom': 17,
-//     'John Doe': 19,
-//     'Bob': 18,
-// };
-
-// const getAdults = usersObj => {
-
-//     // { 'Tom': 17, 'John Doe': 19,} => [['Tom', 17], [ 'John Doe', 19]]
-
-//     const usersArray = Object.entries(usersObj);
-//     // [['Tom', 17], [ 'John Doe', 19]] => [ [ 'John Doe', 19] ]
-//     const filteredUsersArray = usersArray
-//         .filter(user => user[1] >= 18);
-
-//     // [ [ 'John Doe', 19] ]  => ['John Doe']
-//     const usersNames = filteredUsersArray
-//         .map(user => user[0]);
-
-//     return usersNames;
-// }
-
-// console.log(getAdults(users));
-
-
-const getAdults = usersObj => Object.entries(usersObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
-
-console.log(getAdults(users));
-const getAdults = usersObj => Object.entries(usersObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
-
-
+console.log(getAdults(obj));
