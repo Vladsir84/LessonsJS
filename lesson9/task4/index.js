e4ff47d839067057a05f2c3073012c7521dfd80f
John Doe <vperedz@gmail.com>
2020-03-19 16:54:34 +0200

    add

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -1,3 +1,22 @@
-const getPeople = obj => Object.values(obj)
-    .reduce((acc, item) => acc.concat(item), [])
-    .map(room => room.name);
+const rooms = {
+    room1: [
+        {name: 'room1 name1'},
+        {name: 'room1 name2'},
+        {name: 'room1 name3'},
+        {name: 'room1 name4'},
+    ],
+    room2: [
+        {name: 'room2 name1'},
+        
+    ],
+    room3: [
+        {name: 'room3 name1'},
+        {name: 'room3 name2'},
+        {name: 'room3 name3'},
+    ],
+
+}
+
+const getPeople = (rooms) => Object.entries(rooms)
+.reduce((room, name) => room.concat(name), [])
+console.log(getPeople(rooms));
