42468a912be23d549bd2b61a55018b23f269bcee
John Doe <vperedz@gmail.com>
2020-03-23 16:04:02 +0200

    add

--- lesson17/task2/index.js
+++ lesson17/task2/index.js
@@ -1,35 +1,15 @@
-<<<<<<< HEAD
-// callbackPrompt
-
 export const callbackPrompt = {
     message: 'Tell me your number',
     showPrompt() {
-
-        const phoneNumber = prompt(this.message);
+        const phoneNumber = prompt(this.message)
         console.log(phoneNumber);
     },
     showDeferredPrompt(ms) {
-        console.log(this);
-        setTimeout(this.showPrompt.bind(this), ms);
+        setTimeout(() => this.showPrompt.bind(this), ms);
+
+
     }
+
 };
 
-callbackPrompt.showDeferredPrompt(1000);
-=======
-// callbackPrompt
-
-export const callbackPrompt = {
-    message: 'Tell me your number',
-    showPrompt() {
-
-        const phoneNumber = prompt(this.message);
-        console.log(phoneNumber);
-    },
-    showDeferredPrompt(ms) {
-        console.log(this);
-        setTimeout(this.showPrompt.bind(this), ms);
-    }
-};
-
-callbackPrompt.showDeferredPrompt(1000);
->>>>>>> 26b6e0a874f91094fa4e3f8b163f725064a39ed5
+callbackPrompt.showDeferredPrompt(2000);
