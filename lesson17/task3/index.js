2844e6d1ff8fca8a4135117f68615fcf637a1c57
John Doe <vperedz@gmail.com>
2020-03-23 16:35:32 +0200

    add

--- lesson17/task3/index.js
+++ lesson17/task3/index.js
@@ -1,5 +1,15 @@
-export function defer(func, ms){
+export function defer(func, ms) {
     return function() {
         setTimeout(() => func.call(this, ...arguments), ms);
-    } 
+    }
 }
+
+const user = {
+    name: 'Tom',
+    sayHi() {
+        console.log(`Hi, I'm ${this.name}!`)
+    }
+}
+
+const deferredHii = defer(user.sayHi, 2000);
+deferredHii.call({ name: 'Bob'});
