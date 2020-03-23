fe8d696834ca7ed5369f57c6639d2df511b4ed0b
John Doe <vperedz@gmail.com>
2020-03-23 09:15:05 +0200

    add

--- lesson16/task3/index.js
+++ lesson16/task3/index.js
@@ -1,12 +1,14 @@
-export function createArrayOfFunctions(num) {
-    if (!num) return [];
-    if (typeof num !== 'number') return null;
-   
-    let arr = [];
-    for (let i = 0; i < num; i++) {
-        arr[i] = function() {
-            return i;
-        }
+export const createArrayOfFunctions = (num) => {
+    if (!num) return []
+    if (typeof(num) !== 'number') return null   
+ 
+    let arr1 = [] 
+    for (let i = 0; i < num ; i++) {
+        arr1.push(function(){
+           return i
+        })
     }
     return arr
 }
+
+console.log(createArrayOfFunctions('saww'))
