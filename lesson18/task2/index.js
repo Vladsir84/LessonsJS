af22b62c077862af40065ab2f1a5a43f8e7a183d
John Doe <vperedz@gmail.com>
2020-03-25 16:07:42 +0200

    add

--- lesson18/task2/index.js
+++ lesson18/task2/index.js
@@ -1,9 +1,13 @@
 export const wallet = {
     transactions: [1, 5, 89, 337, 3],
     getMax() {
-        return Math.max(...this.transactions);
+       return Math.max(...this.transactions)
     },
     getMin() {
-        return Math.min(...this.transactions);
+       return Math.min(...this.transactions)
     }
 };
+
+
+console.log(wallet.getMax());
+console.log(wallet.getMin());
