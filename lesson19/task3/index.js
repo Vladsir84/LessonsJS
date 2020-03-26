5e07645bf0a1b26257d401f2a3cdb54859424cd2
John Doe <vperedz@gmail.com>
2020-03-26 17:09:41 +0200

    add

--- lesson19/task3/index.js
+++ lesson19/task3/index.js
@@ -1,13 +1,13 @@
-export default  {
+export default user = {
     firstName: 'John',
     lastName: 'Doe',
     get fullName() {
-        return `${this.firstName} ${this.lastName}`;
+        return `${this.firstName} ${this.lastName}`
     },
     set fullName(value) {
-        let [firstName, lastName] = value.split(' ');
+        const [firstName, lastName] = value.split(' ');
         this.firstName = firstName;
         this.lastName = lastName;
-    },
+    }
 };
 
