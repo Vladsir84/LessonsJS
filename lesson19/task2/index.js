595d64ddd59206cc88ab177e9f2f1af4ce1145b7
John Doe <vperedz@gmail.com>
2020-03-26 17:04:54 +0200

    add

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -1,7 +1,7 @@
 export const getOwnProps = (obj) => {
     let arr = [];
     for (let prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
+        if (typeof obj[prop] !== 'function' && obj.hasOwnProperty(prop)) {
          arr.push(prop)
         }
     }
