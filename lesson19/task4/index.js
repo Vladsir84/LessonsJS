c86b0c1670ca4ef69b1b517cbeb181e219927407
John Doe <vperedz@gmail.com>
2020-03-27 15:43:46 +0200

    add

--- lesson19/task4/index.js
+++ lesson19/task4/index.js
@@ -1,31 +1,21 @@
-
 export function User(name, age) {
     this.name = name;
     this.age = age;
 }
-
-User.prototype.sayHi = function () {
+User.prototype.sayHi = function() {
     console.log(`Hi, I am ${this.name}`);
-}
-
-User.prototype.requestNewPhoto = function () {
+};
+User.prototype.requestNewPhoto = function() {
     console.log(`New photo request was sent for ${this.name}`);
-}
-
-User.prototype.setAge = function () {
-    if (age >= 25) {
-        console.log(`New photo request was sent for ${this.name}`);
-        return age;
-    }
+};
+User.prototype.setAge = function(age) {
     if (age < 0) {
         return false;
     }
-    
-}
-
-// const user1 = new User('Tom', 17);
-// const user2 = new User('Bob', 21);
-
-// console.log(user1);
-
-// user1.sayHi();
+    if (age >= 0) {
+        this.age = age;
+        if (this.age >= 25) 
+        console.log(`New photo request was sent for ${this.name}`);
+        return age;
+    }
+};
