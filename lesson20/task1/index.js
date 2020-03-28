5eec60c3a60f5eed78b96bc4d97b2330e58d9205
John Doe <vperedz@gmail.com>
2020-03-28 17:03:12 +0200

    add

--- lesson20/task1/index.js
+++ lesson20/task1/index.js
@@ -3,22 +3,34 @@ export class User {
         this.name = name;
         this.age = age;
     }
+
+    
     static createEmpty() {
-        return {
-            name: "",
+        return user = {
+            name: '',
             age: null,
-        }
     }
+}
     sayHi() {
         console.log(`Hi, I am ${this.name}`);
     }
+
     requestNewPhoto() {
         console.log(`New photo request was sent for ${this.name}`);
     }
+
     setAge(value) {
-        if (value < 0) return false;
+        if (value < 0) {
+            return false;
+        }
         this.age = value;
-        if (this.age >= 25) this.requestNewPhoto();
+        if (age >= 25) {
+            this.requestNewPhoto();
+        }
         return value;
     }
 }
+
+const user1 = new User('Tom', 17);
+const user2 = new User('Bob', 32);
+console.log(User.compare(user1, user2));
