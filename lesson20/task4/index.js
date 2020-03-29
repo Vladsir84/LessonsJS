44875cb241758ecfb841ef0c2be709d2f7d1e304
John Doe <vperedz@gmail.com>
2020-03-29 13:02:44 +0300

    add

--- lesson20/task4/index.js
+++ lesson20/task4/index.js
@@ -1,25 +1,26 @@
 export class Order {
     constructor(price, city, type) {
         this.id = Math.random() + '';
-        this.dateCreated = new Date();
-        this.isConfirmed = false;
         this.price = price;
+        this.dateCreated = null;
+        this.isConfirmed = false;
         this.city = city;
         this.type = type;
     }
 
     checkPrice() {
-        return this.price > 1000 ? true : false;
+     return this.price > 1000 ? true : false
     }
 
     confirmOrder() {
-        this.isConfirmed = true;
-        this.dateConfirmed = new Date();
+      this.isConfirmed = true;
+      this.dateCreated = new Date;
     }
 
-    isValidType() {
-        if (this.type === "Buy" || this.type === "Sell") {
-            return true;
-        } else return false;
+    isValidType(Buy, Sell) {
+       if (this.type === Buy || this.type === Sell){
+           return true
+       }
+      return false; 
     }
 }
