ca170f477b960f705df0ad20023aa6394291b52e
John Doe <vperedz@gmail.com>
2020-03-30 18:37:16 +0300

    add

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,6 +1,5 @@
 export const setButton = (buttonText) => {
     const body = document.querySelector('body');
-    // buttonText = '<button>button text</button>';
-    body.innerHTML = buttonText;
-    body.textContent = buttonText;
+    body.innerHTML = `<button>${buttonText}</button>`;
+    body.textContent =  `<button>${buttonText}</button>`;
 }
