316906aa409f554258c82ae68068ea986d2f98b8
John Doe <vperedz@gmail.com>
2020-03-30 12:49:24 +0300

    add

---
+++ lesson21/task2/index.js
@@ -0,0 +1,9 @@
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+}
+
+export const getInputElement = () => {
+    const inputElem = document.querySelector('.input');
+    console.dir(inputElem);
+}
