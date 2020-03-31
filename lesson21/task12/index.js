4edc6f5c4ab2fe40f20db4d42b9af37536a986dc
John Doe <vperedz@gmail.com>
2020-03-31 14:18:39 +0300

    add

--- lesson21/task12/index.js
+++ lesson21/task12/index.js
@@ -1,12 +1,8 @@
 export const squaredNumbers = () => {
-    const classElem = document.querySelector('.number[data-number = "5"]');
-    const classElem1 = document.querySelector('.number[data-number = "7"]');
-    const classElem2 = document.querySelector('.number[data-number = "-3"]');
-    // classElem.dataset.number = '5';
-    // classElem1.dataset.number = 7;
-    // classElem2.dataset.number = -3;
-    classElem.dataset.squaredNumber = '25';
-    classElem1.dataset.squaredNumber = '49';
-    classElem2.dataset.squaredNumber = '9';
-  
+    const classElements = document.querySelectorAll('.number');
+    
+    for (let item of classElements) {
+       const squaredElement = item.getAttribute('data-number') * item.getAttribute('data-number');
+       item.dataset.squaredNumber = squaredElement
+    }
 }
