3ac343e220fc9cbff5931ff9bbd022e2a1fc8f1e
John Doe <vperedz@gmail.com>
2020-03-31 16:10:22 +0300

    add

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,4 +1,7 @@
 export const getSection = (num) => {
     const classElem = document.querySelector('span[data-number="${num}"]');
-   return classElem.closest('[data-section="two"]')
+    const elem = classElem.closest('div');
+    const closestElem = elem.getAttribute('data-section');
+
+    return closestElem
 }
