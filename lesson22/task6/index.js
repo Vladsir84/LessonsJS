3a8e8f3bf150fd699bb754622f200b3ba597fa0f
John Doe <vperedz@gmail.com>
2020-04-01 10:26:38 +0300

    add

---
+++ lesson22/task6/index.js
@@ -0,0 +1,6 @@
+const inputElem = document.querySelector('.search__input');
+const buttonElem = document.querySelector('.search__btn');
+
+buttonElem.addEventListener('click', () => {
+   console.log(inputElem.value), true }
+);
