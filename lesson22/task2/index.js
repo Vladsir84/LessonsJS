dee2bb09bcf89ab93de0f65c16a524f33218c86a
John Doe <vperedz@gmail.com>
2020-04-02 17:17:22 +0300

    add

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -1,8 +1,10 @@
 const tasks = [
+    { text: 'Buy milk', done: false },
     { text: 'Pick up Tom from airport', done: false },
     { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
     { text: 'Buy meat', done: true },
-];
+  ];
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
