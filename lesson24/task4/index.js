9f55b393ca20e76a02cb252b7780ef43cfa82334
John Doe <vperedz@gmail.com>
2020-04-06 19:13:24 +0300

    add

--- lesson24/task4/index.js
+++ lesson24/task4/index.js
@@ -1,13 +1,13 @@
 let students = [
-    {name:'Tom', birthDate:'01/15/2010'},
-    {name:'Ben', birthDate:'01/17/2008'},
-    {name:'Sam', birthDate:'03/15/2010'},
+    { name: 'Tom', birthDate: '01/15/2010' },
+    { name: 'Ben', birthDate: '01/17/2008' },
+    { name: 'Sam', birthDate: '03/15/2010' },
 ]
 
 export function studentsBirthDays(students) {
-  const arr = students.slice().sort((a, b) => (new Date(a.birthDate)) - 
-   (new Date(b.birthDate)));
-   return arr;
+    const arr = students.slice().sort((a, b) => (new Date(a.birthDate)) -
+        (new Date(b.birthDate)));
+    return arr;
 };
 
 console.log(studentsBirthDays(students));
