5cc2c99e64d11047c8c91e7143a456895b82c725
John Doe <vperedz@gmail.com>
2020-04-06 17:26:37 +0300

    add

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -1,9 +1,9 @@
-export const getDiff = (startDate, endDate) => {
-    const result = Math.abs(endDate - startDate);
-    const seconds = parseInt((result / 1000) % 60);
-    const minutes = parseInt((result / (1000 * 60)) % 60);
-    const hours = parseInt((result / (1000 * 60 * 60)) % 24);
-    const days = parseInt(result / (1000 * 60 * 60 * 24));
-    
+function getDiff(startDate, endDate) {
+    const diffInTime = Math.abs(endDate - startDate) / 1000;
+    const days = Math.floor(diffInTime / 60 / 60 / 24);
+    const hours = Math.floor(diffInTime  / 60 / 60 % 24);
+    const minutes = Math.floor(diffInTime  / 60 % 60);
+    const seconds = Math.floor(diffInTime  % 60);
+   
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-};
+}
