cedb55013ea8e185ed488e34e9e49ef405be4f69
John Doe <vperedz@gmail.com>
2020-04-08 18:34:48 +0300

    add

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,18 +1,15 @@
 export const getLocalStorageData = () => {
     return Object.entries(localStorage)
-    .reduce((acc, [key, value]) => {
-        let newValue;
-        try {
-          newValue = JSON.parse(value) ;
-        } catch(e) {
-           newValue = value;
-        }
-    
-    return {
-        ...acc,
-        [key]: JSON.parse(value),       
-    };    
-    },{});
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value);
+            } catch (e) {
+                newValue = value;
+            }
+            return {
+                ...acc,
+                [key]: newValue,
+            };
+        }, {});
 };
-
-console.log(getLocalStorageData());
