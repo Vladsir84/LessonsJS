10ef7673fdb47c3f4aba1a4fda7c65a93433eb2b
John Doe <vperedz@gmail.com>
2020-04-10 13:42:01 +0300

    add

--- lesson28/task4/index.js
+++ lesson28/task4/index.js
@@ -2,15 +2,14 @@ const add3 = value => value + 3;
 const mult2 = value => value * 2;
 const div4 = value => value / 4;
 
-
 export const compose = (...funcs) => value => {
-    return funcs.reduce((acc, func) => func(acc), value);
+      return  funcs.reduce((acc, func) => func(acc), value)
 };
 
 const doEverything = compose(
-    add3,
-    mult2,
-    div4,
+     add3,
+     mult2,
+     div4,    
 );
 
 console.log(doEverything(3));
