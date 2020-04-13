a997612c2f03917c0ef14ddd605c97b9f46965f4
John Doe <vperedz@gmail.com>
2020-04-13 08:58:36 +0300

    add

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -1,4 +1,4 @@
-export const addImage = (imgSrc, callback) => {
+export function addImage (imgSrc, callback) {
     const imgElem = document.createElement('img');
     imgElem.setAttribute('alt', 'My Photo');
     imgElem.src = imgSrc;
