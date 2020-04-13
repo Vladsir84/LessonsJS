195d63c59a13b39a626358632698efe6b4b8f5ff
John Doe <vperedz@gmail.com>
2020-04-13 15:22:56 +0300

    add

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,22 +1,18 @@
-function getRandomTime(min, max) {
-    let random = min + Math.random() * (max + 1 - min);
-    return Math.floor(random) * 1000;
-}
-
 export const requestUserData = (userId, callback) => {
-    if (userId === 'broken') {
-        setTimeout(() => callback(null, 'Failed to load user data'), getRandomTime(1, 3));
-        return;
-    }
-    setTimeout(() => callback({
-        name: 'John',
-        age: 17,
-        userId: userId,
-        email: `${userId}@example.com`,
-    }), getRandomTime(1, 3));
+    let random = (Math.random() * (3 - 1) + 1) * 1000;
+    setTimeout(() => {
+
+
+        if (userId === 'broken') {
+            callback(null, 'Failed to load user data');
+            return;
+        }
+
+        callback({
+            userId: userId,
+            email: `${userId}@example.com`,
+        })
+    }, random);
 };
 
-const onFormUserData = (firstArg, data) => {
-    if (firstArg === null) return data;
-    return firstArg;
-};
+
