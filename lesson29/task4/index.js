097e8c17016160c15aae584ed4d8aa6ff3af1aea
John Doe <vperedz@gmail.com>
2020-04-13 15:24:30 +0300

    add

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,8 +1,6 @@
 export const requestUserData = (userId, callback) => {
     let random = (Math.random() * (3 - 1) + 1) * 1000;
     setTimeout(() => {
-
-
         if (userId === 'broken') {
             callback(null, 'Failed to load user data');
             return;
