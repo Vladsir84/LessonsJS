50865c1f420cb59e4cdb7f2ba8e44235495da30d
John Doe <vperedz@gmail.com>
2020-04-14 08:52:53 +0300

    add

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,20 +1,27 @@
 export const addImage = imgSrc => {
-            const p = new Promise((resolve, reject) => {
-                const imgElem = document.createElement('img');
-                imgElem.setAttribute('alt', 'My Photo');
-                imgElem.src = imgSrc;
-                const containerElem = document.querySelector('.page');
-                containerElem.append(imgElem);
-
-                const onImageLoaded = () => {
-                    const { width, height } = imgElem;
-                    resolve({ width, height });
-                };
-
-                imgElem.addEventListener('load', onImageLoaded);
-
-                imgElem.addEventListener('error', () => reject('Image load failed'));
-            });
-
-            return p;
+    const p = new Promise((resolveCb, rejectCb) => {
+        const imgElem = document.createElement('img');
+        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.src = imgSrc;
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
+      
+        const onImageLoaded = () => {
+            const { width, height } = imgElem;
+            resolveCb ({ width, height });
         };
+        
+        imgElem.addEventListener('load', onImageLoaded);
+        imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')));
+    });
+
+    return p; 
+};  
+
+const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson+13/hw1/nature2.jpeg'
+
+const resultPromise = addImage(imgSrc);
+
+resultPromise.then(data => console.log(data));
+resultPromise.catch(error => console.log(error));
+
