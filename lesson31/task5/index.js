b7d909e370d4227d3b4ec29f42f80ad9f311c55e
John Doe <vperedz@gmail.com>
2020-04-15 10:17:05 +0300

    add

--- lesson31/task5/index.js
+++ lesson31/task5/index.js
@@ -1,5 +1,5 @@
 export const delay = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms), 3000);
 }
 
-delay(3000).then(() => console.log('Done'));
+// delay(3000).then(() => console.log('Done'));
