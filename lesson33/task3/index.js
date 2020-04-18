e40273ebfce8f0c3630d547154d8c18eabc748b4
John Doe <vperedz@gmail.com>
2020-04-18 16:31:47 +0300

    add

--- lesson33/task3/index.js
+++ lesson33/task3/index.js
@@ -2,7 +2,7 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
 
-export function getTasksList(baseUrl) {
+export function getTasksList() {
     return fetch(baseUrl)
        .then( response => {
           return response.json()
