f2fd356fe24707ed17f2696f74692892e84b9ac0
John Doe <vperedz@gmail.com>
2020-04-21 19:29:37 +0300

    add

--- lesson35/task3/gateways.js
+++ lesson35/task3/gateways.js
@@ -1,18 +1,19 @@
 export const fetchUserData = userName => {
     return fetch(`https://api.github.com/users/${userName}`)
-        .then(response => {
+        .then(response => { 
             if (response.ok) {
-                return response.json()
-            }
+            return response.json()
+            }    
             throw new Error('Failed to load data');
         });
 };
 
 export const fetchRepositories = url =>
     fetch(url)
-    .then(response => {
+    .then(response => { 
         if (response.ok) {
-            return response.json()
-        }
+        return response.json()
+        }    
         throw new Error('Failed to load data');
     });
+
