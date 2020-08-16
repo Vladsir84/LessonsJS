8b27e1c5a7f7a575ad8535ac85b90368410d84cd
John Doe <vperedz@gmail.com>
2020-08-16 17:42:58 +0300

    add

--- lesson6/task7/index.js
+++ lesson6/task7/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sortAsc(array) {
     for (let i = 0; i < array.length - 1; i++) {
         for (let j = 0; j < array.length - 1 - i; j++) {
@@ -25,32 +24,4 @@ function sortDesc(array) {
         }
     }
     return array;
-=======
-function sortAsc(array) {
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 0; j < array.length - 1 - i; j++) {
-            if (array[j + 1] < array[j]) {
-                let t = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = t;
-            }
-        }
-    }
-    return array;
-}
-
-
-
-function sortDesc(array) {
-    for (let j = array.length - 1; j > 0; j--) {
-        for (let i = 0; i < j; i++) {
-            if (array[i] < array[i + 1]) {
-                let t = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = t;
-            }
-        }
-    }
-    return array;
->>>>>>> 26b6e0a874f91094fa4e3f8b163f725064a39ed5
 }
