402b6493c563d7e668be22d80339a90641338bac
John Doe <vperedz@gmail.com>
2020-10-01 19:49:45 +0300

    add

--- lesson6/task18/index.js
+++ lesson6/task18/index.js
@@ -1,8 +1,9 @@
 let arr = [1,2,3,4,5]
 
 function getSubArray(arr, n) {
-  let subArr = arr.slice(n)
-   return subArr
+  if (arr == null) return false;
+  if (n == null) return arr[0]
+  return arr.slice(0, n);
 }
 
 console.log(getSubArray(arr, 2))
